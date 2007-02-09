94956b19f20d004f3365ce05d23242e28d3b0140
Bill Keese <bill@dojotoolkit.org>
2007-02-09 03:05:50 +0000

    port Textbox from dojo/ to dijit/
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@7258 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ form/Textbox.js
@@ -0,0 +1,69 @@
+dojo.provide("dijit.form.Textbox");
+
+dojo.require("dojo.lang.common");
+dojo.require("dojo.string.extras");
+dojo.require("dojo.i18n.common");
+
+dojo.require("dijit.base.FormElement");
+dojo.require("dijit.base.TemplatedWidget");
+
+dojo.declare(
+	"dijit.form.Textbox",
+	[dijit.base.FormElement, dijit.base.TemplatedWidget],
+	{
+		// summary:
+		//		A generic textbox field.
+		//		Serves as a base class to derive more specialized functionality in subclasses.
+
+		//	trim: Boolean
+		//		Removes leading and trailing whitespace if true.  Default is false.
+		trim: false,
+
+		//	uppercase: Boolean
+		//		Converts all characters to uppercase if true.  Default is false.
+		uppercase: false,
+
+		//	lowercase: Boolean
+		//		Converts all characters to lowercase if true.  Default is false.
+		lowercase: false,
+
+		//	ucFirst: Boolean
+		//		Converts the first character of each word to uppercase if true.
+		ucFirst: false,
+
+		//	digit: Boolean
+		//		Removes all characters that are not digits if true.  Default is false.
+		digit: false,
+		
+		templatePath: dojo.uri.moduleUri("dijit.form", "templates/Textbox.html"),
+	
+		postCreate: function() {
+			// assign value programatically in case it has a quote in it
+			this.textbox.value = this.value;
+			this.filter();
+		},
+
+		filter: function() {
+			// summary: Apply various filters to textbox value
+			if (this.trim) {
+				this.textbox.value = this.textbox.value.replace(/(^\s*|\s*$)/g, "");
+			} 
+			if (this.uppercase) {
+				this.textbox.value = this.textbox.value.toUpperCase();
+			} 
+			if (this.lowercase) {
+				this.textbox.value = this.textbox.value.toLowerCase();
+			} 
+			if (this.ucFirst) {
+				this.textbox.value = dojo.string.capitalize(this.textbox.value);
+			} 
+			if (this.digit) {
+				this.textbox.value = this.textbox.value.replace(/\D/g, "");
+			} 
+		},
+	
+		// event handlers, you can over-ride these in your own subclasses
+		onfocus: function() {},
+		onblur: function() { this.filter(); }
+	}
+);
