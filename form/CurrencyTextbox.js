7f7804ed3bda053627a5a11a42b21621f973800b
Adam Peller <github@peller.org>
2007-03-08 16:08:10 +0000

    dijit currency widget uses dojo.currency and i18n tables
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@7551 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/CurrencyTextbox.js
+++ form/CurrencyTextbox.js
@@ -1,40 +1,30 @@
 dojo.provide("dijit.form.CurrencyTextbox");
 
-dojo.require("dijit.form.NumberTextbox");
 dojo.require("dojo.i18n.common");
 dojo.require("dojo.experimental");
 dojo.experimental("dijit.form.CurrencyTextbox");
-dojo.require("dojo.i18n.cldr.currencydata");
+dojo.require("dojo.currency");
 
 dojo.declare(
 	"dijit.form.CurrencyTextbox",
 	dijit.form.NumberTextbox,
 	{
 		// summary:
-		//		A validating, serializable, range-bound text box for entering currency
+		//		A validating, serializable, range-bound text box for localized currencies.
+		// constraints object: ...
+		constraints: {},
 
 		// code: String
 		//		the ISO4217 currency code, a three letter sequence like "USD"
 		//		See http://en.wikipedia.org/wiki/ISO_4217
 		currency: "",
 
-		// currencyData: Object?
-		//		localized data for the specified currency including symbol and displayName
-		currencyData: null,
+		regExpGen: dojo.currency.regexp,
+		format: dojo.currency.format,
+		parse: dojo.currency.parse,
 
 		postMixInProperties: function(){
-			if(!this.currencyData){
-				dojo.requireLocalization("dojo.i18n.cldr", this.currency);
-				try{
-				this.currencyData = dojo.i18n.getLocalization("dojo.i18n.cldr", this.currency);
-				}catch(e){
-					// If not found, just provide ISO symbol.
-					this.currencyData = {};
-				}
-				this.currencyData.iso4217 = this.currency;
-			}
-			dojo.lang.mixin(this.constraints, {type: "currency", currency: this.currencyData});
-			dojo.lang.mixin(this.constraints, dojo.i18n.cldr.currencydata.getData(this.currency));
+			this.constraints.currency = this.currency;
 			dijit.form.CurrencyTextbox.superclass.postMixInProperties.apply(this, arguments);
 		}
 	}
