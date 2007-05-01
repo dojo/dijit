425566c8d383e0454f591ef8b189a79703253b0a
Douglas Hays <doughays@dojotoolkit.org>
2007-05-01 14:52:29 +0000

    Fixes #2682.  Removed unused strings from common.js
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@8384 560b804f-0ae3-0310-86f3-f6aa0a117693

--- nls/common.js
+++ nls/common.js
@@ -1,19 +1,5 @@
 ({
 	buttonOk: "OK",
 	buttonCancel: "Cancel",
-	buttonSave: "Save",
-
-	notSet: "Not set",
-	left: "Left",
-	right: "Right",
-	center: "Center",
-	middle: "Middle",
-	top: "Top",
-	bottom: "Bottom",
-
-	//units
-	unitPixel: "pixels",
-	unitPercent: "percent",
-	width: "Width",
-	height: "Height"
+	buttonSave: "Save"
 })
