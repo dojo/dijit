90ffe175310e3b9060aae5a63365e539fd4f20ef
Bill Keese <bill@dojotoolkit.org>
2007-05-03 18:45:54 +0000

    Remove code related to having a hidden <input> box associated w/the calendar (used for form submission). If people want to have a calendar as a form widget, they'll need to make a new widget that extends this one. Refs #2745.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@8411 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Calendar.js
+++ form/Calendar.js
@@ -9,19 +9,16 @@ dojo.require("dijit.base.TemplatedWidget");
 
 dojo.declare(
 	"dijit.form.Calendar",
-	[dijit.base.FormElement, dijit.base.TemplatedWidget],
+	[dijit.base.Widget, dijit.base.TemplatedWidget],
 	{
 		/*
 		summary: 
-			Base class for a stand-alone DatePicker widget 
-			which makes it easy to select a date, or switch by month and/or year. 
-		description: 
-			A stand-alone DatePicker widget that makes it  
-			easy to select a date, or increment by week, month, and/or year. 
-			It is designed to be used on its own, or inside of other widgets to  
-				(see dojo.widget.DropdownDatePicker) or other similar combination widgets. 
+			Base class for an embedded/stand-alone DatePicker widget 
+			which makes it easy to select a date, or switch by month and/or year.
+			It can't be used in a form because it doesn't serialize the date to an
+			<input> field.
 		 	              
-			Dates attributes passed in the `RFC 3339` format:
+			Note that the parser takes all dates attributes passed in the `RFC 3339` format:
 			http://www.faqs.org/rfcs/rfc3339.html (2005-06-30T08:05:00-07:00)
 			so that they are serializable and locale-independent.
 		
@@ -33,33 +30,12 @@ dojo.declare(
 		*/
 		templatePath: dojo.moduleUrl("dijit.form", "templates/Calendar.html"),
 
-		getTextValue: function(){
-		        return this.filter(this.inputNode.value);
-		},
+		// value: Date
+		value: new Date(),
 
-		getValue: function(){
-			return this.parse(this.getTextValue(), this.constraints);
-		},
-
-		setTextValue: function(value){
-			this.inputNode.value = this.filter(value);
-		},
-
-		setValue: function(dateObj){
+		setValue: function(/*Date*/ dateObj){
 			//summary: set the current date and update the UI
-			var d = dateObj;
-			var t = "";
-			if(typeof(d)=="string" && d!=""){
-				t = dojo.date.serial.fromRfc3339(d);			
-			}else if(typeof(d)=="object"){
-				t = new Date(d);
-			}
-			if(typeof(t)=="object"){
-				this.value = new Date(t);
-				this.value.setHours(0,0,0,0);
-			}else{
-				this.value = "";
-			}
+			this.value = dateObj;
 		
 			if(this.selectedNode){
 				this._removeClass(this.selectedNode,this.classNames.selectedDate);
@@ -72,26 +48,11 @@ dojo.declare(
 				this._preInitUI((this.value=="")?this.curMonth:this.value,false,true);
 			}
 			this.clickedNode=null;
-			this.setTextValue(this.format(value, this.constraints)); //FIXME: value is undefined
-			dijit.form.Calendar.superclass.setValue.call(this,value); //FIXME: value is undefined
 		},
 
-		format: function(/* String */ value, /* Object */ constraints){
-			// summary: Replacable function to convert a value to a properly formatted string
-			return value;
-		},
-
-		parse: function(/* String */ value, /* Object */ constraints){
-			// summary: Replacable function to convert a formatted string to a value
-			return value;
-		},
 		postCreate: function(){
 			dijit.form.Calendar.superclass.postCreate.apply(this);
-			// get the node for which the background color will be updated
-			if (typeof this.nodeWithBorder != "object"){
-				this.nodeWithBorder = this.inputNode;
-			}
-			this.weekTemplate = this.calendarWeekTemplate.parentNode.removeChild(this.calendarWeekTemplate); //PORT leak?
+			this.weekTemplate = this.calendarWeekTemplate.parentNode.removeChild(this.calendarWeekTemplate);
 			this._preInitUI(this.value ? this.value : this.today, false, true); //init UI with date selected ONLY if user supplies one
 
 			// Insert localized day names in the template
@@ -106,80 +67,14 @@ dojo.declare(
 			}
 		},
 
-		filter: function(val){
-			// summary: Return string format of date
-			dojo.date.toRfc3339(new Date(val),'date');
-			return val;
-		},
-	
-		focus: function(){
-			// summary: if the widget wants focus, then focus the textbox
-			this.inputNode.focus();
-		},
-
-		// event handlers, you can over-ride these in your own subclasses
-		onfocus: function(){ 
-			this._addClass(this.nodeWithBorder,"dojoInputFieldFocused"); 
-		},
-		onblur: function(){ 
-			this._removeClass(this.nodeWithBorder,"dojoInputFieldFocused"); 
-			this.setValue(this.getValue()); 
-		},
-		
-		// value: String|Date
-		//	form value property if =='today' will be today's date
-		value: "", 
-
-		// name: String
-		// 	name of the form element
-		name: "",
-
-		// weekStartsOn: Integer
-		//	adjusts the first day of the week 0==Sunday..6==Saturday
-		weekStartsOn: "",
-
-		// dayWidth: String
-		//	how to render the names of the days in the header. see dojo.date.getDayNames
-		dayWidth: 'narrow',
-
+		// TODO: do we need this level of indirection?
 		classNames:{
-		// summary:
-		//	stores a list of class names that may be overriden
-		//	TODO: this is not good; can't be adjusted via markup, etc. since it's an array
 			previous: "dojopreviousMonth",
 			current: "dojocurrentMonth",
 			next: "dojonextMonth",
 			currentDate: "dojocurrentDate",
 			selectedDate: "dojoselectedDate"
 		},
-
-		postMixInProperties: function(){
-			// summary: see dojo.widget.DomWidget
-			dijit.form.Calendar.superclass.postMixInProperties.apply(this, arguments);
-			this.today = new Date();
-			this.today.setHours(0,0,0,0);
-			if(typeof(this.value)=='string'&&this.value.toLowerCase()=='today'){
-				this.value = new Date();
-			}else if(this.value && (typeof this.value=="string") && (this.value.split("-").length > 2)){
-				this.value = dojo.date.fromRfc3339(this.value);
-				this.value.setHours(0,0,0,0);
-			}
-		},
-		
-		getValue: function(){
-			// summary: return current date in RFC 3339 format
-			return dojo.date.toRfc3339(new Date(this.value),'date'); /*String*/
-		},
-
-		getDate: function(){
-			// summary: return current date as a Date object
-			return this.value; /*Date*/
-		},
-
-		setDate: function(/*Date|String*/rfcDate){
-			//summary: set the current date from RFC 3339 formatted string or a date object, synonymous with setDate
-			this.setValue(rfcDate);
-		},			
 
 		_preInitUI: function(dateObj,initFirst,initUI){
 			/*
