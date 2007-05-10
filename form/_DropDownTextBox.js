ab6767e1bc97e4eff215ea2eb486808a1a0277ca
Alex Russell <slightlyoff@chromium.org>
2007-05-10 12:22:52 +0000

    earlier checkins seem to have completely busted InlineEditBox. This seems to fix. Refs #2885, #2309
    Also, we didn't write the style guide because we were looking for something to do one sunny saturday. Compliance is not optional. Continued variances may result in commit privs being revoked. You have been warned.
    
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@8530 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/_DropDownTextBox.js
+++ form/_DropDownTextBox.js
@@ -6,33 +6,33 @@ dojo.require("dijit.base.Widget");
 //dojo.require("dijit.form.ValidationTextbox");
 
 dojo.declare(
-			"dijit.form._DropDownTextBox",
+	"dijit.form._DropDownTextBox",
 	null,
-{
+	{
 		// summary:
 		//		Text box with drop down
-	templatePath: dojo.moduleUrl("dijit.form", "templates/AutoCompleter.html"),
-	// link to the master popup widget
-	popupWidget:null,
-	// the actual node to pop up
-	optionsListNode:null,
-	// how many items to display
-	visibleCount:1,
-	_arrowPressed: function(){
-		if(this.disabled) return;
-		dojo.addClass(this.downArrowNode, "dojoArrowButtonPushed");
-	},
-
-	_arrowIdle: function(){
-		if(this.disabled) return;
-		if(this.popupWidget.aroundwidget!=this){
-			dojo.removeClass(this.downArrowNode, "dojoArrowButtonPushed");
-		}
-	},
-	arrowClicked: function(){
-		
-			// summary: callback when arrow is clicked
+		templatePath: dojo.moduleUrl("dijit.form", "templates/AutoCompleter.html"),
+		// link to the master popup widget
+		popupWidget:null,
+		// the actual node to pop up
+		optionsListNode:null,
+		// how many items to display
+		visibleCount:1,
+		_arrowPressed: function(){
 			if(this.disabled) return;
+			dojo.addClass(this.downArrowNode, "dojoArrowButtonPushed");
+		},
+
+		_arrowIdle: function(){
+			if(this.disabled) return;
+			if(this.popupWidget.aroundwidget!=this){
+				dojo.removeClass(this.downArrowNode, "dojoArrowButtonPushed");
+			}
+		},
+
+		arrowClicked: function(){
+			// summary: callback when arrow is clicked
+			if(this.disabled){ return; }
 			this.focus();
 			if(this.popupWidget.isShowingNow){
 				this._hideResultList();
@@ -41,65 +41,71 @@ dojo.declare(
 					// on the arrow it means they want to see more options
 				this._openResultList();
 			}
-	},
-	postCreate:function(){
-		this.popupWidget=dijit.form._DropDownTextBox.MasterPopup;
-		// create the popup
-		this.optionsListNode=document.createElement('div');
-		dojo.addClass(this.optionsListNode, 'dojoMenu');
-		this.optionsListNode.setAttribute("tabindex", -1);
-		this.optionsListNode.style.display = "none";
-		this.optionsListNode.style.position="absolute";
-		this.optionsListNode.style.overflow="scroll";
-		//dijit.form._DropDownTextBox.superclass.postCreate.apply(this, arguments);
-	},
-	_clearResultList: function(){
-		// detach instead of delete
-		if(this.optionsListNode.innerHTML){
-			this.optionsListNode.innerHTML = "";  // browser natively knows how to collect this memory
-		}
-	},
-	
-	_hideResultList: function(){
-		this.popupWidget.close(this);
-		this._arrowIdle();
-		//this._clearResultList();
-	},
-	_openResultList:function(){
-		// summary:
-		// any code that needs to happen before the popup appears
-		// creating the optionsListNode contents etc.
-		this._showResultList();
-	},
-	onblur:function(){
-			// removeClass dojoInputFieldFocused
-		dojo.removeClass(this.nodeWithBorder, "dojoInputFieldFocused");
-		if(!this.popupWidget.isShowingNow)	this.setValue(this.getValue());
-		// sometimes the tooltip gets stuck; confused by dropdown
-		dijit.MasterTooltip.hide();
-	},
-	onkeypress:function() {
+		},
+
+		postCreate:function(){
+			this.popupWidget = dijit.form._DropDownTextBox.MasterPopup;
+			// create the popup
+			var oln = this.optionsListNode = document.createElement('div');
+			dojo.addClass(oln, 'dojoMenu');
+			oln.setAttribute("tabindex", -1);
+			oln.style.display = "none";
+			oln.style.position="absolute";
+			oln.style.overflow="scroll";
+			//dijit.form._DropDownTextBox.superclass.postCreate.apply(this, arguments);
+		},
+
+		_clearResultList: function(){
+			// detach instead of delete
+			if(this.optionsListNode.innerHTML){
+				this.optionsListNode.innerHTML = "";  // browser natively knows how to collect this memory
+			}
+		},
 		
-	},
-	focus: function(){
-		try{
-			this.textbox.focus();
-		}catch (e){
+		_hideResultList: function(){
+			this.popupWidget.close(this);
+			this._arrowIdle();
+			//this._clearResultList();
+		},
+
+		_openResultList:function(){
+			// summary:
+			//		any code that needs to happen before the popup appears creating
+			//		the optionsListNode contents etc.
+			this._showResultList();
+		},
+
+		onblur:function(){
+
+			// removeClass dojoInputFieldFocused
+			dojo.removeClass(this.nodeWithBorder, "dojoInputFieldFocused");
+			if(!this.popupWidget.isShowingNow)	this.setValue(this.getValue());
+			// sometimes the tooltip gets stuck; confused by dropdown
+			dijit.MasterTooltip.hide();
+		},
+
+		onkeypress:function(){
+			// FIXME: no op?
+		},
+
+		focus: function(){
+			try{
+				this.textbox.focus();
+			}catch(e){
 				// element isn't focusable if disabled, or not visible etc - not easy to test for.
-		};
-	},
-	_showResultList: function(){
+			};
+		},
+		_showResultList: function(){
 			// Our dear friend IE doesnt take max-height so we need to calculate that on our own every time
- 		
-		// optionsListNode could either be a widget or a node
-		// if it is a widget, it will have a domNode
-		// otherwise, it will have children
-		var childs = this.optionsListNode.domNode ? [this.optionsListNode.domNode]: this.optionsListNode.childNodes;
-		this.popupWidget.open(this.optionsListNode.domNode ? this.optionsListNode.domNode : this.optionsListNode, this);
-		if(childs.length){
+			
+			// optionsListNode could either be a widget or a node
+			// if it is a widget, it will have a domNode
+			// otherwise, it will have children
+			var childs = this.optionsListNode.domNode ? [this.optionsListNode.domNode]: this.optionsListNode.childNodes;
+			this.popupWidget.open(this.optionsListNode.domNode ? this.optionsListNode.domNode : this.optionsListNode, this);
+			if(childs.length){
 				var visibleCount = Math.min(childs.length,this.maxListLength);
-				with(this.optionsListNode.domNode ? this.optionsListNode.domNode.style : this.optionsListNode.style)
-				{
+				with(this.optionsListNode.domNode ? this.optionsListNode.domNode.style : this.optionsListNode.style){
 					display="";
 					if(visibleCount == childs.length){
 						//no scrollbar is required, so unset height to let browser calcuate it,
@@ -113,80 +119,79 @@ dojo.declare(
 							calcheight=windowheight-coords.y;
 						}
 						height=calcheight+"px";
-						
 					}
 					width = childs[0].width+"px";
 				}
-				
-			}
-			else{
+			}else{
 				this._hideResultList();
 			}
-	
-	}
-});
-dojo.declare(
-			"dijit.form._DropDownTextBox.Popup",
-	dijit.base.Widget,
-{
-	// summary:
-	//	prevents onBlur timers from accidentally closing another widget's popup, or upsetting PopupManager
+		}
+	});
+	dojo.declare(
+				"dijit.form._DropDownTextBox.Popup",
+		dijit.base.Widget,
+	{
+		// summary:
+		//	prevents onBlur timers from accidentally closing another widget's popup, or upsetting PopupManager
 
-	isShowingNow:false,
-	aroundWidget:null,
-	open:function(/*Node*/ optionsListNode, /*Widget*/ aroundWidget){
-		// summary: open node optionsListNode around widget aroundwidget
-		if(!this.isShowingNow){
-			aroundWidget._arrowPressed();
-			document.body.appendChild(optionsListNode);
-			this.aroundWidget=aroundWidget;
-			// trickery for popupmanager
-			this.domNode=optionsListNode;
-			this.isShowingNow=true;
-			setTimeout(dojo.hitch(this, "_open"), 10);
-			//this._open();
-		}
-	},
-	_open:function() {
-		dijit.util.PopupManager.openAround(this.aroundWidget.textbox, this,{'BL':'TL', 'TL':'BL'}, [0,0]);
-	},
-	close:function(/*[Widget || Boolean]*/ aroundWidget){
-		// summary: close popup belonging to aroundWidget
-		if(this.isShowingNow) {
-			// did PopupManager initiate the close?
-			// if not, call close through PopupManager
-			if(this.aroundWidget==aroundWidget) {
-				try {
-					dijit.util.PopupManager.close();
-				}
-				catch(e) {
-					// Popup already closed; a blur event is calling this close
-					// call close manually to clean up
-					this.close(true);
+		isShowingNow:false,
+		aroundWidget:null,
+
+		open:function(/*Node*/ optionsListNode, /*Widget*/ aroundWidget){
+			// summary: open node optionsListNode around widget aroundwidget
+			if(!this.isShowingNow){
+				aroundWidget._arrowPressed();
+				document.body.appendChild(optionsListNode);
+				this.aroundWidget=aroundWidget;
+				// trickery for popupmanager
+				this.domNode=optionsListNode;
+				this.isShowingNow=true;
+				setTimeout(dojo.hitch(this, "_open"), 10);
+				//this._open();
+			}
+		},
+
+		_open:function(){
+			dijit.util.PopupManager.openAround(this.aroundWidget.textbox, this,{'BL':'TL', 'TL':'BL'}, [0,0]);
+		},
+
+		close:function(/*[Widget || Boolean]*/ aroundWidget){
+			// summary: close popup belonging to aroundWidget
+			if(this.isShowingNow){
+				// did PopupManager initiate the close?
+				// if not, call close through PopupManager
+				if(this.aroundWidget==aroundWidget){
+					try{
+						dijit.util.PopupManager.close();
+					}catch(e){
+						// Popup already closed; a blur event is calling this close
+						// call close manually to clean up
+						this.close(true);
+					}
+				}else if(this.isShowingNow && (typeof(aroundWidget)=="boolean")){
+					// if PopupManager calls close, aroundWidget is a boolean
+					// then it is ok to clean up
+					
+					this.isShowingNow=false;
+					// detach optionsListNode in case it is a widget
+					dojo.body().removeChild(this.domNode);
+					this.domNode=null;
+					this.aroundwidget=null;
 				}
 			}
-			// if PopupManager calls close, aroundWidget is a boolean
-			// then it is ok to clean up
-			else if(this.isShowingNow&&typeof(aroundWidget)=="boolean") {
-				
-				this.isShowingNow=false;
-				// detach optionsListNode in case it is a widget
-				document.body.removeChild(this.domNode);
-				this.domNode=null;
-				this.aroundwidget=null;
-			}
-		}
-	},
-	processKey:function(/*Event*/ e){
+		},
+
+		processKey:function(/*Event*/ e){
 			// required by PopupManager
 			// PopupManager eats the user input otherwise
-		return false;
+			return false;
+		}
 	}
-}
-
 );
-dojo.addOnLoad(function(){
-	if(!dijit.form._DropDownTextBox.MasterPopup){	
-		dijit.form._DropDownTextBox.MasterPopup=new dijit.form._DropDownTextBox.Popup(null, document.createElement('div'));
+
+// dojo.addOnLoad() throws things on the end of the onload stack. We want to be on the front.
+dojo._loaders.unshift(function(){
+	if(!dijit.form._DropDownTextBox.MasterPopup){
+		dijit.form._DropDownTextBox.MasterPopup = new dijit.form._DropDownTextBox.Popup({}, document.createElement("div"));
 	}
 });
