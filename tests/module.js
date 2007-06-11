e837e3180255a8f3a1c6220de1a8a7d9e65f0dd1
Bill Keese <bill@dojotoolkit.org>
2007-06-11 15:53:28 +0000

    Tests for getChildren() Patch from Simon Bates (CLA on file). Refs #3282
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@9018 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -3,6 +3,7 @@ dojo.provide("dijit.tests.module");
 dojo
 try{
 	dojo.require("dijit.tests.widgetsInTemplate");
+	dojo.require("dijit.tests.Container");
 //	dojo.require("tests.currency");
 }catch(e){
 	doh.debug(e);
