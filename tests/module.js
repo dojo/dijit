179fee3bce18b6feddd145fb0147b11968a9f068
Bill Keese <bill@dojotoolkit.org>
2007-07-17 09:11:44 +0000

    Tests for _Templated base class. Refs #2976.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@9682 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -1,6 +1,7 @@
 dojo.provide("dijit.tests.module");
 
 try{
+	dojo.require("dijit.tests._Templated");
 	dojo.require("dijit.tests.widgetsInTemplate");
 	dojo.require("dijit.tests.Container");
 	dojo.require("dijit.tests.layout.ContentPane");
