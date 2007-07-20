0e0fc339921b94d0ea530b3d0702c4c5defc9d18
Bill Keese <bill@dojotoolkit.org>
2007-07-20 09:28:14 +0000

    oops, missed one.  refs #3819
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@9730 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base.js
+++ _base.js
@@ -5,6 +5,7 @@ dojo.require("dijit._base.manager");
 dojo.require("dijit._base.place");
 dojo.require("dijit._base.popup");
 dojo.require("dijit._base.scroll");
+dojo.require("dijit._base.sniff");
 dojo.require("dijit._base.typematic");
 dojo.require("dijit._base.wai");
 dojo.require("dijit._base.window");
