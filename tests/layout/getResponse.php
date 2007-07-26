07b77a297d37b7237901aa98db489c58ee4d3dde
Fredrik Johansson <mumme@dojotoolkit.org>
2007-07-26 15:04:02 +0000

    Quiting a E_NOTICE message that causes ContentPane.html test to fail in some environments thanks to simonjb for finding it! refs #3856
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@9805 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/layout/getResponse.php
+++ tests/layout/getResponse.php
@@ -28,16 +28,25 @@
 				echo "<div dojotype='dijit.TestWidget'>Delayed setHref test</div>
 					  <div dojotype='dijit.TestWidget'>Delayed by $delay sec.</div>";
 				break;
+			case 3:
+				echo "IT WAS the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way -- in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only";
+				break;
+			case 4:
+				echo "There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.";
+				break;
+			case 5:
+				echo "It was the year of Our Lord one thousand seven hundred and seventy- five. Spiritual revelations were conceded to England at that favoured period, as at this. Mrs. Southcott had recently attained her five-and- twentieth blessed birthday, of whom a prophetic private in the Life Guards had heralded the sublime appearance by announcing that arrangements were made for the swallowing up of London and Westminster. Even the Cock-lane ghost had been laid only a round dozen of years, after rapping out its messages, as the spirits of this very year last past (supernaturally deficient in originality) rapped out theirs. Mere messages in the earthly order of events had lately come to the English Crown and People, from a congress of British subjects in America:";
+				break;
 			default:
 				echo "unknown messId:{$_GET['messId']}";
 		}
 	}
 
-	if($_GET['bounceGetStr']){
+	if(isset($_GET) &&  $_GET['bounceGetStr']){
 		echo "<div id='bouncedGetStr'>{$_SERVER["QUERY_STRING"]}</div>";
 	}
 
-	if($_GET['message']){
+	if(isset($_GET) && $_GET['message']){
 		echo $_GET['message'];
 	}
 
