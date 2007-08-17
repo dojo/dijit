c28357e0158a2bb9e23fcf8ee50259f1f0221299
Alex Russell <slightlyoff@chromium.org>
2007-08-17 20:07:03 +0000

    declarations were still not handling ctor-level functions correctly. This fixes. Refs #4199
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@10229 560b804f-0ae3-0310-86f3-f6aa0a117693

--- Declaration.js
+++ Declaration.js
@@ -21,26 +21,28 @@ dojo.declare(
 			var scripts = dojo.query("> script[type^='dojo/']", src).orphan();
 			var srcType = src.nodeName;
 
+			var propList = this.defaults||{};
 
 			this.mixins = this.mixins.length ? 
 				dojo.map(this.mixins, dojo.getObject) : 
 				[ dijit._Widget, dijit._Templated ];
-			this.mixins.push(function(){
-				scripts.forEach(dojo.hitch(dojo.parser, "_wireUpMethod", this));
-			});
 
-			var propList = this.defaults||{};
 			if(preambles.length){
 				// we only support one preamble. So be it.
 				propList.preamble = dojo.parser._functionFromScript(preambles[0]);
 			}
 			propList.widgetsInTemplate = true;
-			propList.templateString = "<"+srcType+">"+src.innerHTML+"</"+srcType+">";
+			propList.templateString = "<"+srcType+" class='"+src.className+"'>"+src.innerHTML+"</"+srcType+">";
 
 			// strip things so we don't create stuff under us in the initial setup phase
 			dojo.query("[dojoType]", src).forEach(function(node){
 				node.removeAttribute("dojoType");
 			});
+			scripts.forEach(function(s){
+				if(!s.getAttribute("event")){
+					this.mixins.push(dojo.parser._functionFromScript(s));
+				}
+			}, this);
 
 			// create the new widget class
 			dojo.declare(
@@ -48,6 +50,13 @@ dojo.declare(
 				this.mixins,
 				propList
 			);
+
+			var wcp = dojo.getObject(this.widgetClass).prototype;
+			scripts.forEach(function(s){
+				if(s.getAttribute("event")){
+					dojo.parser._wireUpMethod(wcp, s);
+				}
+			});
 		}
 	}
 );
