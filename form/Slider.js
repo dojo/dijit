4c40c5064d8cb14cfaf9b7682ad58ecaa471a485
Douglas Hays <doughays@dojotoolkit.org>
2007-09-27 18:20:56 +0000

    Fixes #3685. Labels may be specified by 1 of the following methods: 1) by overriding the getLabels() function 2) via the labels array 3) as <li> children 4) numeric computation using minimum(default 0), maximum(default 1), count(default 3), numericMargin(default 0), constraints(default {pattern:"#%:})
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@10636 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Slider.js
+++ form/Slider.js
@@ -4,6 +4,7 @@ dojo.require("dijit.form._FormWidget");
 dojo.require("dijit._Container");
 dojo.require("dojo.dnd.move");
 dojo.require("dijit.form.Button");
+dojo.require("dojo.number");
 
 dojo.declare(
 	"dijit.form.HorizontalSlider",
@@ -297,6 +298,22 @@ dojo.declare("dijit.form.HorizontalRuleLabels", dijit.form.HorizontalRule,
 	//	Array of text labels to render - evenly spaced from left-to-right or bottom-to-top
 	labels: [],
 
+	// numericMargin: Integer
+	//	Number of generated numeric labels that should be rendered as '' on the ends when labels[] are not specified
+	numericMargin: 0,
+
+	// numericMinimum: Integer
+	//	Leftmost label value for generated numeric labels when labels[] are not specified
+	minimum: 0,
+
+	// numericMaximum: Integer
+	//	Rightmost label value for generated numeric labels when labels[] are not specified
+	maximum: 1,
+
+	// constraints: object
+	//	pattern, places, lang, et al (see dojo.number) for generated numeric labels when labels[] are not specified
+	constraints: {pattern:"#%"},
+
 	_positionPrefix: '<div class="RuleLabelContainer HorizontalRuleLabelContainer" style="left:',
 	_labelPrefix: '"><span class="RuleLabel HorizontalRuleLabel">',
 	_suffix: '</span></div>',
@@ -309,20 +326,34 @@ dojo.declare("dijit.form.HorizontalRuleLabels", dijit.form.HorizontalRule,
 		return this._positionPrefix + this._calcPosition(pos) + this._positionSuffix + this.labelStyle + this._labelPrefix + this.labels[ndx] + this._suffix;
 	},
 
-	postMixInProperties: function(){
-		this.inherited('postMixInProperties', arguments);
-		if(!this.labels.length){
+	getLabels: function(){
+		// summary: user replaceable function to return the labels array
+
+		// if the labels array was not specified directly, then see if <li> children were
+		var labels = this.labels;
+		if(!labels.length){
 			// for markup creation, labels are specified as child elements
-			this.labels = dojo.query("> li", this.srcNodeRef).map(function(node){
+			labels = dojo.query("> li", this.srcNodeRef).map(function(node){
 				return String(node.innerHTML);
 			});
 		}
-		this.srcNodeRef.innerHTML="";
+		this.srcNodeRef.innerHTML = '';
+		// if the labels were not specified directly and not as <li> children, then calculate numeric labels
+		if(!labels.length && this.count > 1){
+			var start = this.minimum;
+			var inc = (this.maximum - start) / (this.count-1);
+			for (var i=0; i < this.count; i++){
+				labels.push((i<this.numericMargin||i>=(this.count-this.numericMargin))? '' : dojo.number.format(start, this.constraints));
+				start += inc;
+			}
+		}
+		return labels;
 	},
 
-	postCreate: function(){
+	postMixInProperties: function(){
+		this.inherited('postMixInProperties', arguments);
+		this.labels = this.getLabels();
 		this.count = this.labels.length;
-		this.inherited('postCreate', arguments);
 	}
 });
 
