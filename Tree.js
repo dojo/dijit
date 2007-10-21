101d62dbe7bc3e5da823d86d14fa84ea4b1f7320
Karl Tiedt <ktiedt@gmail.com>
2007-10-21 21:37:46 +0000

    per discussion with Bill, this meets his criteria fixes #4817
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11034 560b804f-0ae3-0310-86f3-f6aa0a117693

--- Tree.js
+++ Tree.js
@@ -61,15 +61,10 @@ dojo.declare(
 		this._setExpando(false);	
 	},
 
-	_updateItemClasses: function(item, oldValue, newValue) {
+	_updateItemClasses: function(item) {
 		// summary: set appropriate CSS classes for item (used to allow for item updates to change respective CSS)
-		dojo.forEach([this.iconNode, this.labelNode], function(n){
-			if(dojo.hasClass(n,oldValue)) {
-				dojo.removeClass(n,oldValue);
-			}
-		});
-		dojo.addClass(this.iconNode, this.tree.getIconClass(item));
-		dojo.addClass(this.labelNode, this.tree.getLabelClass(item));
+		this.iconNode.style.className = "dijitInline dijitTreeIcon " + this.getIconClass(item);
+		this.labelNode.style.className = "dijitTreeLabel " + this.getLabelClass(item);
 	},
 	
 	_updateLayout: function(){
@@ -861,14 +856,14 @@ dojo.declare(
 		}
 	},
 
-	_onSetItem: function(/*Object*/ item, /*attribute-name-string*/ attribute, /*object | array*/ oldValue, /*object | array*/ newValue){
+	_onSetItem: function(/*Object*/ item){
 		//summary: set data event  on an item in the store
-		var identity = this.store.getIdentity(item),
+		var identity = this.store.getIdentity(item),act
 		node = this._itemNodeMap[identity];
 
 		if (node){
 			node.setLabelNode(this.getLabel(item));
-			node._updateItemClasses(item, oldValue, newValue);
+			node._updateItemClasses(item);
 		}
 	}
 });
