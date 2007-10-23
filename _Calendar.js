b600f6b02529fb22ef8b33f127d1cc7bfa2cbcd6
Adam Peller <github@peller.org>
2007-10-23 13:09:20 +0000

    Ignore mouse-ups.  Fixes #4810
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11071 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _Calendar.js
+++ _Calendar.js
@@ -130,7 +130,9 @@ dojo.declare(
 			// Set up repeating mouse behavior
 			var _this = this;
 			var typematic = function(nodeProp, dateProp, adj){
-				dijit.typematic.addMouseListener(_this[nodeProp], _this, function(){ _this._adjustDisplay(dateProp, adj); }, 0.8, 500);
+				dijit.typematic.addMouseListener(_this[nodeProp], _this, function(count){
+					if(count >= 0){ _this._adjustDisplay(dateProp, adj); }
+				}, 0.8, 500);
 			};
 			typematic("incrementMonth", "month", 1);
 			typematic("decrementMonth", "month", -1);
