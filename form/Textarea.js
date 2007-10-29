9f475255f1ef558359e8808d8d9ae64c49fedb1d
Douglas Hays <doughays@dojotoolkit.org>
2007-10-29 23:18:39 +0000

    Fixes #4918.  Corrected syntax error.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11196 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Textarea.js
+++ form/Textarea.js
@@ -22,7 +22,7 @@ dojo.declare(
 	templateString: (dojo.isIE || dojo.isSafari || dojo.isMozilla) ?
 				((dojo.isIE || dojo.isSafari) ? '<fieldset id="${id}" class="dijitInline dijitInputField dijitTextArea" dojoAttachPoint="styleNode" waiRole="presentation"><div dojoAttachPoint="editNode,focusNode,eventNode" dojoAttachEvent="onpaste:_changing,oncut:_changing" waiRole="textarea" style="text-decoration:none;_padding-bottom:16px;display:block;overflow:auto;" contentEditable="true"></div>'
 					: '<span id="${id}" class="dijitReset">'+
-					'<iframe src="javascript:<html><head><title>${_iframeEditTitle}</title></head><body><script>if(window.frameElement){var _postCreate=window.frameElement.postCreate;if(_postCreate)_postCreate();}</script></body></html>"'+
+					'<iframe src="javascript:<html><head><title>${_iframeEditTitle}</title></head><body><script>var _postCreate=window.frameElement?window.frameElement.postCreate:null;if(_postCreate)_postCreate();</script></body></html>"'+
 							' dojoAttachPoint="iframe,styleNode" dojoAttachEvent="onblur:_onIframeBlur" class="dijitInline dijitInputField dijitTextArea"></iframe>')
 				+ '<textarea name="${name}" value="${value}" dojoAttachPoint="formValueNode" style="display:none;"></textarea>'
 				+ ((dojo.isIE || dojo.isSafari) ? '</fieldset>':'</span>')
