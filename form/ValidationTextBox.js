b6ed3ddc6fe21c2228c3a6f11a51bf4f5e2d3edc
Adam Peller <github@peller.org>
2007-12-05 16:13:43 +0000

    Back out [11710], refs #4511.  Doesn't tolerate bad input at all for some reason.  Also, it would be nice to tolerate (but not require) symbols and separators in input.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11713 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/ValidationTextBox.js
+++ form/ValidationTextBox.js
@@ -94,9 +94,6 @@ dojo.declare(
 				if(!message && !isValid){
 					message = this.getErrorMessage(true);
 				}
-				if(this.editOptions){
-					this.textbox.value = (this.format(this.getValue(), dojo.mixin(dojo.mixin({}, this.editOptions), this.constraints)));
-				}
 			}
 			this.displayMessage(message);
 		},
