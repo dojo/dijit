04cf36e311be61a66299b6984d51b27406f137db
Adam Peller <github@peller.org>
2007-12-05 03:03:17 +0000

    Move supplemental docs to their respective projects. Refs #982
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11702 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ resources/_modules.js
@@ -0,0 +1,39 @@
+// dijit fallback for key summaries otherwise not covered by the doc parser
+// never, ever include this file. really.
+
+dijit.demos = {
+	// summary: Home of the official dijit demo code
+};
+
+dijit.dijit = {
+	// summary: A roll-up for common dijit methods
+	// description:
+	//	A rollup file for the build system including the core and common
+	//	dijit files.
+	//	
+	// example:
+	// | <script type="text/javascript" src="js/dojo/dijit/dijit.js"></script>
+	//
+};
+
+dijit["dijit-all"] = { 
+	// summary: A rollup that includes every dijit. You probably don't need this.
+};
+
+dijit.form = {
+	// summary: Form and input related widgets
+};
+
+dijit.layout = {
+	// summary: Layout related widgets
+};
+
+dijit.nls = { 
+	// summary: i18n translations for dijit
+};
+
+dijit.registry = {
+	// summary: A list of widgets on a page.
+	// description: Is an instance of dijit.WidgetSet
+};
+
