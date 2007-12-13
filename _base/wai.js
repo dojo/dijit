e25b6efd43b12a650078c77c5a41bf02caeacf0a
Simon Bates <simonjb@dojotoolkit.org>
2007-12-13 16:02:57 +0000

    Fixed up the wai.js function docs. Fixes #5366.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@11810 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/wai.js
+++ _base/wai.js
@@ -3,18 +3,17 @@ dojo.provide("dijit._base.wai");
 dijit.wai = {
 	onload: function(){
 		// summary:
-		//		Function that detects if we are in high-contrast mode or not,
-		//		and sets up a timer to periodically confirm the value.
-		//		figure out the background-image style property
-		//		and apply that to the image.src property.
-		// description:
-		//		This must be a named function and not an anonymous
-		//		function, so that the widget parsing code can make sure it
-		//		registers its onload function after this function.
-		//		DO NOT USE "this" within this function.
+		//		Detects if we are in high-contrast mode or not,
+		//		and sets up a timer to periodically check.
 
+		// This must be a named function and not an anonymous
+		// function, so that the widget parsing code can make sure it
+		// registers its onload function after this function.
+		// DO NOT USE "this" within this function.
+
+		var div;
 		// create div for testing if high contrast mode is on or images are turned off
-		var div = document.createElement("div");
+		div = document.createElement("div");
 		div.id = "a11yTestNode";
 		div.style.cssText = 'border: 1px solid;'
 			+ 'border-color:red green;'
@@ -49,7 +48,8 @@ if(dojo.isIE || dojo.isMoz){	// NOTE: checking in Safari messes things up
 dojo.mixin(dijit,
 {
 	hasWaiRole: function(/*Element*/ elem){
-		// Summary: Return true if elem has a role attribute and false if not.
+		// summary: Determines if an element has a role.
+		// returns: true if elem has a role attribute and false if not.
 		if(elem.hasAttribute){
 			return elem.hasAttribute("role");
 		}else{
@@ -58,8 +58,10 @@ dojo.mixin(dijit,
 	},
 
 	getWaiRole: function(/*Element*/ elem){
-		// Summary: Return the role of elem or an empty string if
-		//		elem does not have a role.
+		// summary: Gets the role for an element.
+		// returns:
+		//		The role of elem or an empty string if elem
+		//		does not have a role.
 		var value = elem.getAttribute("role");
 		if(value){
 			var prefixEnd = value.indexOf(":");
@@ -70,7 +72,9 @@ dojo.mixin(dijit,
 	},
 
 	setWaiRole: function(/*Element*/ elem, /*String*/ role){
-		// Summary: Set the role on elem. On Firefox 2 and below, "wairole:" is
+		// summary: Sets the role on an element.
+		// description:
+		//		On Firefox 2 and below, "wairole:" is
 		//		prepended to the provided role value.
 		if(dojo.isFF && dojo.isFF < 3){
 			elem.setAttribute("role", "wairole:"+role);
@@ -80,19 +84,23 @@ dojo.mixin(dijit,
 	},
 
 	removeWaiRole: function(/*Element*/ elem){
-		// Summary: Removes the role attribute from elem.
+		// summary: Removes the role from an element.
 		elem.removeAttribute("role");
 	},
 
 	hasWaiState: function(/*Element*/ elem, /*String*/ state){
-		// Summary: Return true if elem has a value for the given state and
-		//		false if it does not.
+		// summary: Determines if an element has a given state.
+		// description:
 		//		On Firefox 2 and below, we check for an attribute in namespace
 		//		"http://www.w3.org/2005/07/aaa" with a name of the given state.
-		//		On all other browsers, we check for an attribute called
-		//		"aria-"+state.
+		//		On all other browsers, we check for an attribute
+		//		called "aria-"+state.
+		// returns:
+		//		true if elem has a value for the given state and
+		//		false if it does not.
 		if(dojo.isFF && dojo.isFF < 3){
-			return elem.hasAttributeNS("http://www.w3.org/2005/07/aaa", state);
+			var val = elem.hasAttributeNS("http://www.w3.org/2005/07/aaa", state);
+			return val;
 		}else{
 			if(elem.hasAttribute){
 				return elem.hasAttribute("aria-"+state);
@@ -103,14 +111,18 @@ dojo.mixin(dijit,
 	},
 
 	getWaiState: function(/*Element*/ elem, /*String*/ state){
-		// Summary: Return the value of the requested state on elem
-		//		or an empty string if elem has no value for state.
+		// summary: Gets the value of a state on an element.
+		// description:
 		//		On Firefox 2 and below, we check for an attribute in namespace
 		//		"http://www.w3.org/2005/07/aaa" with a name of the given state.
 		//		On all other browsers, we check for an attribute called
 		//		"aria-"+state.
+		// returns:
+		//		The value of the requested state on elem
+		//		or an empty string if elem has no value for state.
 		if(dojo.isFF && dojo.isFF < 3){
-			return elem.getAttributeNS("http://www.w3.org/2005/07/aaa", state);
+			var val = elem.getAttributeNS("http://www.w3.org/2005/07/aaa", state);
+			return val;
 		}else{
 			var value =  elem.getAttribute("aria-"+state);
 			return value ? value : "";
@@ -118,7 +130,8 @@ dojo.mixin(dijit,
 	},
 
 	setWaiState: function(/*Element*/ elem, /*String*/ state, /*String*/ value){
-		// Summary: Set state on elem to value.
+		// summary: Sets a state on an element.
+		// description:
 		//		On Firefox 2 and below, we set an attribute in namespace
 		//		"http://www.w3.org/2005/07/aaa" with a name of the given state.
 		//		On all other browsers, we set an attribute called
@@ -132,7 +145,8 @@ dojo.mixin(dijit,
 	},
 
 	removeWaiState: function(/*Element*/ elem, /*String*/ state){
-		// Summary: Removes the given state from elem.
+		// summary: Removes a state from an element.
+		// description:
 		//		On Firefox 2 and below, we remove the attribute in namespace
 		//		"http://www.w3.org/2005/07/aaa" with a name of the given state.
 		//		On all other browsers, we remove the attribute called
