505e4959fff03d471892d8043e1501cd2cf4ddf8
Heng Liu <liucougar@gmail.com>
2008-02-05 21:25:14 +0000

    fixes #5771
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@12293 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/RichText.js
+++ _editor/RichText.js
@@ -2,6 +2,7 @@ dojo.provide("dijit._editor.RichText");
 
 dojo.require("dijit._Widget");
 dojo.require("dijit._editor.selection");
+dojo.require("dijit._editor.html");
 dojo.require("dojo.i18n");
 dojo.requireLocalization("dijit", "Textarea");
 
@@ -283,7 +284,7 @@ dojo.declare("dijit._editor.RichText", dijit._Widget, {
 //					});
 //				}
 		}else{
-			html = this._preFilterContent(this.getNodeChildrenHtml(this.domNode));
+			html = this._preFilterContent(dijit.html.getChildrenHtml(this.domNode));
 			this.domNode.innerHTML = '';
 		}
 		if(html == ""){ html = "&nbsp;"; }
@@ -1240,7 +1241,7 @@ dojo.declare("dijit._editor.RichText", dijit._Widget, {
 					dom = ef(dom);
 				});
 			}
-			ec = this.getNodeChildrenHtml(dom);
+			ec = dijit.html.getChildrenHtml(dom);
 		}else{
 			ec = dom;
 		}
@@ -1265,104 +1266,19 @@ dojo.declare("dijit._editor.RichText", dijit._Widget, {
 		saveTextarea.value += this._SEPARATOR + this.name + ":" + this.getValue();
 	},
 
-
 	escapeXml: function(/*String*/str, /*Boolean*/noSingleQuotes){
-		//summary:
-		//		Adds escape sequences for special characters in XML: &<>"'
-		//		Optionally skips escapes for single quotes
-		str = str.replace(/&/gm, "&amp;").replace(/</gm, "&lt;").replace(/>/gm, "&gt;").replace(/"/gm, "&quot;");
-		if(!noSingleQuotes){
-			str = str.replace(/'/gm, "&#39;");
-		}
-		return str; // string
+		dojo.deprecated('dijit.Editor::escapeXml is deprecated','use dijit.html.escapeXml instead', 2);
+		return dijit.html.escapeXml(str,noSingleQuotes);
 	},
 
 	getNodeHtml: function(/* DomNode */node){
-		var output;
-		switch(node.nodeType){
-			case 1: //element node
-				output = '<'+node.tagName.toLowerCase();
-
-				//store the list of attributes and sort it to have the
-				//attributes appear in the dictionary order
-				var attrarray = [];
-				if(dojo.isIE){
-					var s = node.outerHTML;
-					s = s.substr(0,s.indexOf('>'));
-					s = s.replace(/(?:['"])[^"']*\1/g, '');//to make the following regexp safe
-					var reg = /([^\s=]+)=/g;
-					var m, key;
-					while((m = reg.exec(s)) != undefined){
-						key=m[1];
-						if(key.substr(0,3) != '_dj'){
-							if(key == 'src' || key == 'href'){
-								if(node.getAttribute('_djrealurl')){
-									attrarray.push([key,node.getAttribute('_djrealurl')]);
-									continue;
-								}
-							}
-							if(key=='style'){
-								attrarray.push([key, node.style.cssText.toLowerCase()]);
-							}else{
-								attrarray.push([key, key=='class'?node.className:node.getAttribute(key)]);
-							}
-						}
-					}
-				}else{
-					var attr, i=0, attrs = node.attributes;
-					while((attr=attrs[i++])){
-						//ignore all attributes starting with _dj which are
-						//internal temporary attributes used by the editor
-						if(attr.name.substr(0,3) != '_dj' /*&&
-							(attr.specified == undefined || attr.specified)*/){
-							var v = attr.value;
-							if(attr.name == 'src' || attr.name == 'href'){
-								if(node.getAttribute('_djrealurl')){
-									v = node.getAttribute('_djrealurl');
-								}
-							}
-							attrarray.push([attr.name,v]);
-						}
-					}
-				}
-				attrarray.sort(function(a,b){
-					return a[0]<b[0]?-1:(a[0]==b[0]?0:1);
-				});
-				i=0;
-				while((attr=attrarray[i++])){
-					output += ' '+attr[0]+'="'+attr[1]+'"';
-				}
-				if(node.childNodes.length){
-					output += '>' + this.getNodeChildrenHtml(node)+'</'+node.tagName.toLowerCase()+'>';
-				}else{
-					output += ' />';
-				}
-				break;
-			case 3: //text
-				// FIXME:
-				output = this.escapeXml(node.nodeValue,true);
-				break;
-			case 8: //comment
-				// FIXME:
-				output = '<!--'+this.escapeXml(node.nodeValue,true)+'-->';
-				break;
-			default:
-				output = "Element not recognized - Type: " + node.nodeType + " Name: " + node.nodeName;
-		}
-		return output;
+		dojo.deprecated('dijit.Editor::getNodeHtml is deprecated','use dijit.html.getNodeHtml instead', 2);
+		return dijit.html.getNodeHtml(node);
 	},
 
 	getNodeChildrenHtml: function(/* DomNode */dom){
-		// summary: Returns the html content of a DomNode and children
-		var out = "";
-		if(!dom){ return out; }
-		var nodes = dom["childNodes"]||dom;
-		var i=0;
-		var node;
-		while((node=nodes[i++])){
-			out += this.getNodeHtml(node);
-		}
-		return out; // String
+		dojo.deprecated('dijit.Editor::getNodeChildrenHtml is deprecated','use dijit.html.getChildrenHtml instead', 2);
+		return dijit.html.getChildrenHtml(dom);
 	},
 
 	close: function(/*Boolean*/save, /*Boolean*/force){
