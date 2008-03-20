664029f12141a02711f45538b2ee986f05340ea2
Tom Trenka <ttrenka@gmail.com>
2008-03-20 15:07:37 +0000

    A fix for the *Really* Temporary Ugly Fix(tm), forcing Safari to resize itself; connect to window.onload instead of using dojo.addOnLoad.  !strict, refs #5626.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13157 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base.js
+++ _base.js
@@ -11,13 +11,13 @@ dojo.require("dijit._base.typematic");
 dojo.require("dijit._base.wai");
 dojo.require("dijit._base.window");
 
-//	TODO: Find a better way of solving this bug!
-//	Ugly-ass hack to solve bug #5626 for 1.1; basically force Safari to re-layout.
+//	FIXME: Find a better way of solving this bug!
 if(dojo.isSafari){
-	dojo.addOnLoad(function(){
-		setTimeout(function(){
-			window.resizeBy(1,0);
-			setTimeout(function(){ window.resizeBy(-1,0); }, 10);
-		}, 10);
+	//	Ugly-ass hack to solve bug #5626 for 1.1; basically force Safari to re-layout.
+	//	Note that we can't reliably use dojo.addOnLoad here because this bug is basically
+	//		a timing / race condition; so instead we use window.onload.
+	dojo.connect(window, "load", function(){
+		window.resizeBy(1,0);
+		setTimeout(function(){ window.resizeBy(-1,0); }, 10);
 	});
 }
