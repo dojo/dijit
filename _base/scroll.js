d3b01e8ad67a65d68c0872764767006da1bb12c8
Douglas Hays <doughays@dojotoolkit.org>
2008-04-14 19:36:57 +0000

    Fixes #6359 for 1.2.  Remove native scrollIntoView() call for Mozilla.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13320 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/scroll.js
+++ _base/scroll.js
@@ -6,22 +6,20 @@ dijit.scrollIntoView = function(/* DomNode */node){
 
 	// don't rely on that node.scrollIntoView works just because the function is there
 	// it doesnt work in Konqueror or Opera even though the function is there and probably
-	// not safari either
+	//	not safari either
+	// native scrollIntoView() causes FF3's whole window to scroll if there is no scroll bar 
+	//	on the immediate parent
 	// dont like browser sniffs implementations but sometimes you have to use it
-	if(dojo.isMozilla){
-		node.scrollIntoView(false);
-	}else{
-		// #6146: IE scrollIntoView is broken
-		// It's not enough just to scroll the menu node into view if
-		// node.scrollIntoView hides part of the parent's scrollbar,
-		// so just manage the parent scrollbar ourselves
-		var parent = node.parentNode;
-		var parentBottom = parent.scrollTop + dojo.marginBox(parent).h; //PORT was getBorderBox
-		var nodeBottom = node.offsetTop + dojo.marginBox(node).h;
-		if(parentBottom < nodeBottom){
-			parent.scrollTop += (nodeBottom - parentBottom);
-		}else if(parent.scrollTop > node.offsetTop){
-			parent.scrollTop -= (parent.scrollTop - node.offsetTop);
-		}
+	// #6146: IE scrollIntoView is broken
+	// It's not enough just to scroll the menu node into view if
+	// node.scrollIntoView hides part of the parent's scrollbar,
+	// so just manage the parent scrollbar ourselves
+	var parent = node.parentNode;
+	var parentBottom = parent.scrollTop + dojo.marginBox(parent).h; //PORT was getBorderBox
+	var nodeBottom = node.offsetTop + dojo.marginBox(node).h;
+	if(parentBottom < nodeBottom){
+		parent.scrollTop += (nodeBottom - parentBottom);
+	}else if(parent.scrollTop > node.offsetTop){
+		parent.scrollTop -= (parent.scrollTop - node.offsetTop);
 	}
 };
