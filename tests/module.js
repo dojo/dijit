cfff45aaecbd18eab4b5fc3840d5ef0977032e40
Bill Keese <bill@dojotoolkit.org>
2008-05-07 03:11:18 +0000

    Remove an unnecessary level of indirection; it's not very useful (IMO) to require every HTML test file to have an associated JS stub. Perhaps composite files at a higher level (_base, form, etc.) would be useful but the current design is more trouble than it's worth.
    Refs #5011 etc.
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13604 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -1,18 +1,25 @@
 dojo.provide("dijit.tests.module");
 
 try{
-	dojo.require("dijit.tests._base.manager");
+	// _base tests
+	doh.registerUrl("dijit.tests._base.manager", dojo.moduleUrl("dijit", "tests/_base/manager.html"));
 	doh.registerUrl("dijit.tests._base.sniffQuirks", dojo.moduleUrl("dijit", "tests/_base/sniffQuirks.html"));
 	doh.registerUrl("dijit.tests._base.sniffStandards", dojo.moduleUrl("dijit", "tests/_base/sniffStandards.html"));
-	dojo.require("dijit.tests._base.viewport");
-	dojo.require("dijit.tests._base.wai");
+	doh.registerUrl("dijit.tests._base.viewport", dojo.moduleUrl("dijit", "tests/_base/viewport.html"));
+	doh.registerUrl("dijit.tests._base.viewportStrict", dojo.moduleUrl("dijit", "tests/_base/viewportStrict.html"));
+	doh.registerUrl("dijit.tests._base.wai", dojo.moduleUrl("dijit", "tests/_base/wai.html"));
 
-	dojo.require("dijit.tests._Templated");
-	dojo.require("dijit.tests.widgetsInTemplate");
-	dojo.require("dijit.tests.Container");
-	dojo.require("dijit.tests.layout.ContentPane");
-	dojo.require("dijit.tests.ondijitclick");
-	dojo.require("dijit.tests.form.Form");
+	// other infrastructure tests
+	doh.registerUrl("dijit.tests._Templated", dojo.moduleUrl("dijit", "tests/_Templated.html"));
+	doh.registerUrl("dijit.tests.widgetsInTemplate", dojo.moduleUrl("dijit", "tests/widgetsInTemplate.html"));
+	doh.registerUrl("dijit.tests.Container", dojo.moduleUrl("dijit", "tests/Container.html"));
+	doh.registerUrl("dijit.tests.ondijitclick", dojo.moduleUrl("dijit", "tests/ondijitclick.html"));
+
+	// form tests
+	doh.registerUrl("dijit.tests.form.Form", dojo.moduleUrl("dijit", "tests/form/Form.html"));
+
+	// layout tests
+	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 30000);
 }catch(e){
 	doh.debug(e);
 }
