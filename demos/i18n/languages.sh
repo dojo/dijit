6f787a6b8bf71f579e711660cb16e63f69388384
Bill Keese <bill@dojotoolkit.org>
2008-05-23 09:01:50 +0000

    Fixes #5082 and more correctly fixes #5033 by using country codes rather than country names. Unfortunately it turns out that 2-letter ISO codes aren't unique (AF is both a continent and a country), so had to construct unique key (based on type and ISO code) manually. No longer using the data inlined into generate.html but could reference it to get URLs for countries if we wanted to, so leaving there for now.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13786 560b804f-0ae3-0310-86f3-f6aa0a117693

--- demos/i18n/languages.sh
+++ demos/i18n/languages.sh
@@ -8,7 +8,7 @@
 echo "["
 for lang in $(ls [a-z][a-z].xml |sed s/.xml//)
 do
-	echo '{ type: "language", iso: "'${lang}'",'
+	echo '{ key: "language:'${lang}'", type: "language", iso: "'${lang}'",'
 
 	# countries that use this language
 	echo 'countries: ['
@@ -18,7 +18,7 @@ do
 	# name of this language (in this language)
 	grep '<language type="'${lang}'"[ >]' ${lang}.xml |head -1 |sed -e 's/.*<language type=".."[^>]*>/name: "/' -e 's/<\/language>/",/'
 
-	# names of other langauges (in this language)
+	# names of other languages (in this language)
 	grep '<language type="..">' ${lang}.xml |sed -e 's/.*<language type=//' -e 's/<\/language>/",/' -e 's/>/: "/' -e 's/-->//'
 	echo '},'
 done
