822e874a48545a46fc2eb616b12c326ce8571094
Bill Keese <bill@dojotoolkit.org>
2008-06-07 07:24:04 +0000

    Put dijitRtl on <html> rather than <body>, and also put combination class names on <html> like dj_ie6-rtl to indicate combination of browser and text direction. Refs #6881.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13910 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/sniff.js
+++ _base/sniff.js
@@ -1,11 +1,18 @@
+//	summary:
+//		Applies pre-set CSS classes to the top-level HTML node, based on:
+// 			- browser (ex: dj_ie)
+//			- browser version (ex: dj_ie6)
+//			- box model (ex: dj_contentBox)
+//			- text direction (ex: dijitRtl)
+//
+//		In addition, browser, browser version, and box model are
+//		combined with an RTL flag when browser text is RTL.  ex: dj_ie-rtl.
+//
+//		Simply doing a require on this module will
+//		establish this CSS.  Modified version of Morris' CSS hack.
+
 dojo.provide("dijit._base.sniff");
 
-// ported from dojo.html.applyBrowserClass (style.js)
-
-//	summary:
-//		Applies pre-set class names based on browser & version to the
-//		top-level HTML node.  Simply doing a require on this module will
-//		establish this CSS.  Modified version of Morris' CSS hack.
 (function(){
 	var d = dojo;
 	var ie = d.isIE;
@@ -30,10 +37,12 @@ dojo.provide("dijit._base.sniff");
 		dj_ff3: maj(ff) == 3
 	}; // no dojo unsupported browsers
 	classes["dj_" + boxModel] = true;
-	
+
+	var html = dojo.doc.documentElement;
+
+	// apply browser, browser version, and box model class names
 	for(var p in classes){
 		if(classes[p]){
-			var html = dojo.doc.documentElement; //TODO browser-specific DOM magic needed?
 			if(html.className){
 				html.className += " " + p;
 			}else{
@@ -41,4 +50,18 @@ dojo.provide("dijit._base.sniff");
 			}
 		}
 	}
+
+	// If RTL mode then add dijitRtl flag plus repeat existing classes
+	// with -rtl extension
+	// (unshift is to make this code run after <body> node is loaded but before parser runs)
+	dojo._loaders.unshift(function(){
+		if(!dojo._isBodyLtr()){
+			html.className += " dijitRtl";
+			for(var p in classes){
+				if(classes[p]){
+					html.className += " " + p+"-rtl";
+				}
+			}
+		}
+	});
 })();
