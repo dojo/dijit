284928cdcdbea28f3331e3c6bab62c2819df8dca
Adam Peller <github@peller.org>
2008-06-21 01:24:33 +0000

    updated translation. Fixes #6907
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@14094 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/nls/pt/FontChoice.js
+++ _editor/nls/pt/FontChoice.js
@@ -5,14 +5,14 @@
 
 	serif: "serif",
 	"sans-serif": "sans-serif",
-	monospaced: "monospaced",
+	monospaced: "monoespaçado",
 	cursive: "cursive",
 	fantasy: "fantasy",
 
 	p: "Parágrafo",
 	h1: "Título",
-	h2: "Subtítulo",
-	h3: "Sub-subtítulo",
+	h2: "Subcabeçalho",
+	h3: "Sub-subcabeçalho",
 	pre: "Pré-formatado",
 
 	1: "xx-small",
@@ -21,5 +21,5 @@
 	4: "medium",
 	5: "large",
 	6: "x-large",
-	7: "xx-large"
+	7: "xx-large "
 })
