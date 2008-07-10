160036f16d252b15c6c8ff93e4f548e6e5745cec
Becky Gibson <becky@dojotoolkit.org>
2008-07-10 19:45:03 +0000

    fixes #6397 Based on recommendations of keyboard style guide, Added support for home/end key to quickly set value to min/max via the keyboard.  Home/end key swallowed if no home/end value specified in order to provide consistent behavior.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@14388 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/NumberSpinner.js
+++ form/NumberSpinner.js
@@ -30,5 +30,19 @@ dojo.declare(
 			newval = this.constraints.min;
 		}
 		return newval;
+	},
+	_onKeyPress: function(e){
+		if((e.charOrCode == dojo.keys.HOME || e.charOrCode == dojo.keys.END) && !e.ctrlKey && !e.altKey){
+			var value = e.charOrCode == dojo.keys.HOME ? this.constraints["min"] : this.constraints["max"];
+			if (value){
+				this.setValue(value,true);
+			}
+			// eat home or end key whether we change the value or not
+			dojo.stopEvent(e);
+			return false;
+		}
+		else{
+			return this.inherited(arguments);
+		}
 	}
 });
