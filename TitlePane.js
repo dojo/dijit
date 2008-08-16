5a676488206bb9f7ed909d0af9cf9272eb2dd8b4
Nathan Toone <nathan@toonetown.com>
2008-08-16 03:15:59 +0000

    Refs #7244 - better documentation of dijit.TitlePane.attr("open") and allow for non-typed comparison of values
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@14870 560b804f-0ae3-0310-86f3-f6aa0a117693

--- TitlePane.js
+++ TitlePane.js
@@ -80,10 +80,12 @@ dojo.declare(
 		this.inherited(arguments);
 	},
 
-	_setOpenAttr: function(open){
+	_setOpenAttr: function(/* Boolean */ open){
 		// summary:
-		//		Hook to make attr("open", ...) work.
-		if(this.open != open){ this.toggle(); }
+		//		Hook to make attr("open", boolean) control the open/closed state of the pane.
+		// open: Boolean
+		//		True if you want to open the pane, false if you want to close it.
+		if(this.open !== open){ this.toggle(); }
 	},
 
 	_setContentAttr: function(content){
