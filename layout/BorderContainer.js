e3830aae8fe14a2ed09e274b3fd5debb5f5ec150
Dustin Machi <dmachi@dojotoolkit.org>
2008-09-10 23:42:45 +0000

    update janky section for resize. refs #7598 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@15216 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/BorderContainer.js
+++ layout/BorderContainer.js
@@ -326,13 +326,13 @@ dojo.declare(
 
 			if(leftSplitter){ leftSplitter.style.height = sidebarHeight; }
 			if(rightSplitter){ rightSplitter.style.height = sidebarHeight; }
-			resizeWidget(this._leftWidget, {h: sidebarHeight}, dim.left);
-			resizeWidget(this._rightWidget, {h: sidebarHeight}, dim.right);
+			resizeWidget(this._leftWidget, dim.left);
+			resizeWidget(this._rightWidget, dim.right);
 
 			if(topSplitter){ topSplitter.style.width = sidebarWidth; }
 			if(bottomSplitter){ bottomSplitter.style.width = sidebarWidth; }
-			resizeWidget(this._topWidget, {w: sidebarWidth}, dim.top);
-			resizeWidget(this._bottomWidget, {w: sidebarWidth}, dim.bottom);
+			resizeWidget(this._topWidget, dim.top);
+			resizeWidget(this._bottomWidget, dim.bottom);
 
 			resizeWidget(this._centerWidget, dim.center);
 		}else{
