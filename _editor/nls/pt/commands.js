9bb09350521dff2265c7c8201c300c04f4b503e1
Adam Peller <github@peller.org>
2008-11-11 00:35:44 +0000

    put pt-br strings in wrong place.  Refs #7691
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.2/dijit@15638 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/nls/pt/commands.js
+++ _editor/nls/pt/commands.js
@@ -37,6 +37,8 @@
 	'formatBlock': 'Estilo de Parágrafo',
 	'fontSize': 'Tamanho da Fonte',
 	'fontName': 'Nome da Fonte',
+	'tabIndent': 'Recuo da Tabulação',
 	/* Error messages */
-	'systemShortcutFF': 'A ação "${0}" está disponível apenas no Mozilla Firefox utilizando um atalho do teclado. Utilize ${1}.'
+	'systemShortcutFF': 'A ação "${0}" está disponível apenas no Mozilla Firefox utilizando um atalho do teclado. Utilize ${1}.',
+	'systemShortcut': 'A ação "${0}" está apenas disponível em seu navegador utilizando um atalho de teclado. Utilize ${1}.'
 })
