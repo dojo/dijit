87d256270f2b0abde2ab28226c10909a10062901
Nathan Toone <nathan@toonetown.com>
2008-11-26 01:30:57 +0000

    Refs #8165 - fix missing parent variable value now that we are cleaning ourselves up
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@15872 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/scroll.js
+++ _base/scroll.js
@@ -74,64 +74,66 @@ dijit.scrollIntoView = function(/* DomNode */node){
 	var element = node;
 	while(element != scrollRoot){
 		parent = element._parent;
-		if(parent.tagName == "TD"){
-			var table = parent._parent._parent._parent; // point to TABLE
-			if(table._offsetParent == element._offsetParent && parent._offsetParent != element._offsetParent){
-				parent = table; // child of TD has the same offsetParent as TABLE, so skip TD, TR, and TBODY (ie. verticalslider)
+		if(parent){
+			if(parent.tagName == "TD"){
+				var table = parent._parent._parent._parent; // point to TABLE
+				if(table._offsetParent == element._offsetParent && parent._offsetParent != element._offsetParent){
+					parent = table; // child of TD has the same offsetParent as TABLE, so skip TD, TR, and TBODY (ie. verticalslider)
+				}
 			}
-		}
-		// check if this node and its parent share the same offsetParent
-		var startIsRelative = element == scrollRoot || (parent._offsetParent != element._offsetParent);
-
-		for(dir in element._start){ // for both x and y directions
-			var otherDir = dir=="H"? "V" : "H";
-			if(rtl && dir=="H" && (dojo.isSafari || dojo.isIE) && parent._clientSize.H > 0){ // scroll starts on the right
-				var delta = parent.scrollWidth - parent._clientSize.H;
-				if(delta > 0){ parent._scroll.H -= delta; } // match FF3 which has cool negative scrollLeft values
+			// check if this node and its parent share the same offsetParent
+			var startIsRelative = element == scrollRoot || (parent._offsetParent != element._offsetParent);
+	
+			for(dir in element._start){ // for both x and y directions
+				var otherDir = dir=="H"? "V" : "H";
+				if(rtl && dir=="H" && (dojo.isSafari || dojo.isIE) && parent._clientSize.H > 0){ // scroll starts on the right
+					var delta = parent.scrollWidth - parent._clientSize.H;
+					if(delta > 0){ parent._scroll.H -= delta; } // match FF3 which has cool negative scrollLeft values
+				}
+				if(dojo.isIE && parent._offsetParent.tagName == "TABLE"){ // make it consistent with Safari and FF3 and exclude the starting TABLE border of TABLE children
+					parent._start[dir] -= parent._offsetParent._borderStart[dir];
+					parent._borderStart[dir] = parent._borderSize[dir] = 0;
+				}
+				if(parent._clientSize[dir] == 0){ // TABLE on Safari3/FF3, and TBODY on IE6/7
+					parent._renderedSize[dir] = parent._clientSize[dir] = parent._child._clientSize[dir];
+					if(rtl && dir=="H"){ parent._start[dir] -= parent._renderedSize[dir]; }
+				}else{
+					parent._renderedSize[dir] -= parent._borderSize[dir] + parent._scrollBarSize[dir];
+				}
+				parent._start[dir] += parent._borderStart[dir];
+	
+				// underflow = visible gap between parent and this node taking scrolling into account
+				// if negative, part of the node is obscured by the parent's beginning and should be scrolled to become visible
+				var underflow = element._start[dir] - (startIsRelative? 0 : parent._start[dir]) - parent._scroll[dir];
+				// if positive, number of pixels obscured by the parent's end
+				var overflow = underflow + element._renderedSize[dir] - parent._renderedSize[dir];
+				var scrollAmount, scrollAttr = (dir=="H")? "scrollLeft" : "scrollTop";
+				// see if we should scroll forward or backward
+				var reverse = (dir=="H" && rtl); // flip everything
+				var underflowScroll = reverse? -overflow : underflow;
+				var overflowScroll = reverse? -underflow : overflow;
+				if(underflowScroll <= 0){
+					scrollAmount = underflowScroll;
+				}else if(overflowScroll <= 0){
+					scrollAmount = 0;
+				}else if(underflowScroll < overflowScroll){
+					scrollAmount = underflowScroll;
+				}else{
+					scrollAmount = overflowScroll;
+				}
+				var scrolledAmount = 0;
+				if(scrollAmount != 0){
+					var oldScroll = parent[scrollAttr];
+					parent[scrollAttr] += reverse? -scrollAmount : scrollAmount; // actually perform the scroll
+					scrolledAmount = parent[scrollAttr] - oldScroll; // in case the scroll failed
+					underflow -= scrolledAmount;
+					overflowScroll -= reverse? -scrolledAmount : scrolledAmount;
+				}
+				parent._renderedSize[dir] = element._renderedSize[dir] + parent._scrollBarSize[dir] - 
+					// check for isScrollable since a nonscrolling parent could be smaller than the child but the child is fully visible
+					((parent._isScrollable[dir] && overflowScroll > 0)? overflowScroll : 0); // only show portion of the parent
+				parent._start[dir] += (underflow >= 0 || !parent._isScrollable[dir])? underflow : 0;
 			}
-			if(dojo.isIE && parent._offsetParent.tagName == "TABLE"){ // make it consistent with Safari and FF3 and exclude the starting TABLE border of TABLE children
-				parent._start[dir] -= parent._offsetParent._borderStart[dir];
-				parent._borderStart[dir] = parent._borderSize[dir] = 0;
-			}
-			if(parent._clientSize[dir] == 0){ // TABLE on Safari3/FF3, and TBODY on IE6/7
-				parent._renderedSize[dir] = parent._clientSize[dir] = parent._child._clientSize[dir];
-				if(rtl && dir=="H"){ parent._start[dir] -= parent._renderedSize[dir]; }
-			}else{
-				parent._renderedSize[dir] -= parent._borderSize[dir] + parent._scrollBarSize[dir];
-			}
-			parent._start[dir] += parent._borderStart[dir];
-
-			// underflow = visible gap between parent and this node taking scrolling into account
-			// if negative, part of the node is obscured by the parent's beginning and should be scrolled to become visible
-			var underflow = element._start[dir] - (startIsRelative? 0 : parent._start[dir]) - parent._scroll[dir];
-			// if positive, number of pixels obscured by the parent's end
-			var overflow = underflow + element._renderedSize[dir] - parent._renderedSize[dir];
-			var scrollAmount, scrollAttr = (dir=="H")? "scrollLeft" : "scrollTop";
-			// see if we should scroll forward or backward
-			var reverse = (dir=="H" && rtl); // flip everything
-			var underflowScroll = reverse? -overflow : underflow;
-			var overflowScroll = reverse? -underflow : overflow;
-			if(underflowScroll <= 0){
-				scrollAmount = underflowScroll;
-			}else if(overflowScroll <= 0){
-				scrollAmount = 0;
-			}else if(underflowScroll < overflowScroll){
-				scrollAmount = underflowScroll;
-			}else{
-				scrollAmount = overflowScroll;
-			}
-			var scrolledAmount = 0;
-			if(scrollAmount != 0){
-				var oldScroll = parent[scrollAttr];
-				parent[scrollAttr] += reverse? -scrollAmount : scrollAmount; // actually perform the scroll
-				scrolledAmount = parent[scrollAttr] - oldScroll; // in case the scroll failed
-				underflow -= scrolledAmount;
-				overflowScroll -= reverse? -scrolledAmount : scrolledAmount;
-			}
-			parent._renderedSize[dir] = element._renderedSize[dir] + parent._scrollBarSize[dir] - 
-				// check for isScrollable since a nonscrolling parent could be smaller than the child but the child is fully visible
-				((parent._isScrollable[dir] && overflowScroll > 0)? overflowScroll : 0); // only show portion of the parent
-			parent._start[dir] += (underflow >= 0 || !parent._isScrollable[dir])? underflow : 0;
 		}
 		element = parent; // now see if the parent needs to be scrolled as well
 	}
