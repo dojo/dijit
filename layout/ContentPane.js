14aeaa04d544381f8c4fa041f44987f0df0c0aff
Bill Keese <bill@dojotoolkit.org>
2008-11-26 11:07:48 +0000

    Fixes #8168: ContentPane wasn't correctly sizing a single layout widget downloaded from an href. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.2/dijit@15877 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/ContentPane.js
+++ layout/ContentPane.js
@@ -212,15 +212,6 @@ dojo.declare(
 		this._setContent(data || "");
 
 		this._isDownloaded = false; // mark that content is from a attr('content') not an attr('href')
-
-		if(this.doLayout != "false" && this.doLayout !== false){
-			this._checkIfSingleChild();
-			if(this._singleChild && this._singleChild.resize){
-				this._singleChild.startup();
-				var cb = this._contentBox || dojo.contentBox(this.containerNode);
-				this._singleChild.resize({w: cb.w, h: cb.h});
-			}
-		}
 	},
 	_getContentAttr: function(){
 		// summary: hook to make attr("content") work
@@ -451,6 +442,15 @@ dojo.declare(
 		delete this._contentSetterParams;
 		
 		if(!isFakeContent){
+			if(this.doLayout != "false" && this.doLayout !== false){
+				this._checkIfSingleChild();
+				if(this._singleChild && this._singleChild.resize){
+					this._singleChild.startup();
+					var cb = this._contentBox || dojo.contentBox(this.containerNode);
+					this._singleChild.resize({w: cb.w, h: cb.h});
+				}
+			}
+
 			this._onLoadHandler(cont);
 		}
 	},
