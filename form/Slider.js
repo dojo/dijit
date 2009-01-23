e1af1555f92d5242a6fe7e833045ec3490cf86bd
Peter E Higgins <dante@dojotoolkit.org>
2009-01-23 03:22:39 +0000

    fixes #8473 - build regexp picks up dojo.require() about anywhere it exists, so be careful. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16455 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Slider.js
+++ form/Slider.js
@@ -1,6 +1,6 @@
 dojo.provide("dijit.form.Slider");
 
-dojo.deprecated("Do dojo.require() HorizontalSlider, VerticalRule, etc, rather than of dijit.form.Slider itself", "", "2.0");
+dojo.deprecated("Call require() for HorizontalSlider / VerticalRule, explicitly rather than 'dijit.form.Slider' itself", "", "2.0");
 
 // For back-compat, remove for 2.0
 dojo.require("dijit.form.HorizontalSlider");
