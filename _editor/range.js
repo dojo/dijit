3d3137f04505f60bc6f74a2569c3e49a0cd43a61
Heng Liu <liucougar@gmail.com>
2009-01-24 08:47:32 +0000

    fixes #8490: simplified code path of IE w3c range compatible layer implementation improved support for some corner cases
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16463 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/range.js
+++ _editor/range.js
@@ -331,46 +331,31 @@ if(!dijit.range._w3c){
 			}
 			return [startnode, startOffset];
 		},
-		setEndPoint: function(range, container, offset){
+		setEndPoint: function(range, container, offset, boundary){
 			//text node
 			var atmrange = range.duplicate(), node, len;
 			if(container.nodeType!=3){ //normal node
-				atmrange.moveToElementText(container);
-				atmrange.collapse(true);
-				if(offset == container.childNodes.length){
-					if(offset > 0){
-						//a simple atmrange.collapse(false); won't work here:
-						//although moveToElementText(node) is supposed to encompass the content of the node,
-						//but when collapse to end, it is in fact after the ending tag of node (collapse to start
-						//is after the begining tag of node as expected)
-						node = container.lastChild;
-						len = 0;
-						while(node && node.nodeType == 3){
-							len += node.length;
-							container = node; //pass through
-							node = node.previousSibling;
-						}
-						if(node){
-							atmrange.moveToElementText(node);
-						}
-						atmrange.collapse(false);
-						offset = len; //pass through
-					}else{ //no childNodes
-						atmrange.moveToElementText(container);
-						atmrange.collapse(true);
-					}
-				}else{
-					if(offset > 0){
-						node = container.childNodes[offset-1];
-						if(node.nodeType==3){
-							container = node;
-							offset = node.length;
+				if(offset > 0){
+					node = container.childNodes[offset-1];
+					if(node.nodeType==3){
+						container = node;
+						offset = node.length;
+						//pass through
+					}else{
+						if(node.nextSibling && node.nextSibling.nodeType==3){
+							container=node.nextSibling;
+							offset=0;
 							//pass through
 						}else{
-							atmrange.moveToElementText(node);
+							atmrange.moveToElementText(node.nextSibling?node:container);
+							var tempnode=node.parentNode.insertBefore(document.createTextNode(' '),node.nextSibling);
 							atmrange.collapse(false);
+							tempnode.parentNode.removeChild(tempnode);
 						}
 					}
+				}else{
+					atmrange.moveToElementText(container);
+					atmrange.collapse(true);
 				}
 			}
 			if(container.nodeType==3){
@@ -398,6 +383,10 @@ if(!dijit.range._w3c){
 				}
 			}
 
+			range.setEndPoint(boundary, atmrange);
+			/*if(tempnode){
+				tempnode.parentNode.removeChild(tempnode);
+			}*/
 			return atmrange;
 		},
 		decomposeTextRange: function(range){
@@ -417,13 +406,13 @@ if(!dijit.range._w3c){
 		},
 		setRange: function(range, startContainter,
 			startOffset, endContainter, endOffset, collapsed){
-			var startrange = dijit.range.ie.setEndPoint(range, startContainter, startOffset);
-			range.setEndPoint('StartToStart', startrange);
-			var endrange=startrange;
+			dijit.range.ie.setEndPoint(range, startContainter, startOffset,'StartToStart');
+
 			if(!collapsed){
-				endrange = dijit.range.ie.setEndPoint(range, endContainter, endOffset);
+				endrange = dijit.range.ie.setEndPoint(range, endContainter, endOffset,'EndToEnd');
+			}else{
+				range.collapse(true);
 			}
-			range.setEndPoint('EndToEnd', endrange);
 
 			return range;
 		}
@@ -442,16 +431,6 @@ dojo.declare("dijit.range.W3CRange",null, {
 			this.endOffset = 0;
 			this.collapsed = true;
 		}
-	},
-	_simpleSetEndPoint: function(node, range, end){
-		var r = (this._body||node.ownerDocument.body).createTextRange();
-		if(node.nodeType!=1){
-			r.moveToElementText(node.parentNode);
-		}else{
-			r.moveToElementText(node);
-		}
-		r.collapse(true);
-		range.setEndPoint(end?'EndToEnd':'StartToStart',r);
 	},
 	_updateInternal: function(){
 		if(this.startContainer !== this.endContainer){
