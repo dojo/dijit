06c8d2f4ae04cb333e3834bb03db2220de4d8752
Bill Keese <bill@dojotoolkit.org>
2009-02-14 00:27:53 +0000

    Fix LinkPane.  Fixes #8648 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16682 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/LinkPane.js
+++ layout/LinkPane.js
@@ -19,14 +19,20 @@ dojo.declare("dijit.layout.LinkPane",
 	// I'm using a template because the user may specify the input as
 	// <a href="foo.html">title</a>, in which case we need to get rid of the
 	// <a> because we don't want a link.
-	templateString: '<div class="dijitLinkPane"></div>',
+	templateString: '<div class="dijitLinkPane" dojoAttachPoint="containerNode"></div>',
 
-	postCreate: function(){
+	postMixInProperties: function(){
 		// If user has specified node contents, they become the title
 		// (the link must be plain text)
 		if(this.srcNodeRef){
 			this.title += this.srcNodeRef.innerHTML;
 		}
-		this.inherited("postCreate",arguments);
+		this.inherited(arguments);
+	},
+
+	_fillContent: function(/*DomNode*/ source){
+		// This method relocates srcNodeRef innerHTML to templated container node,
+		// but in our case the srcNodeRef innerHTML is the title, so shouldn't be
+		// copied
 	}
 });
