d6ea11c99e92a5a10bf700e0aa0a071683cfec35
Douglas Hays <doughays@dojotoolkit.org>
2009-02-15 13:28:39 +0000

    Fixes #8651.  The attribute ^= operator is not working on IE.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16685 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/CheckBox.js
+++ form/CheckBox.js
@@ -126,7 +126,7 @@ dojo.declare(
 			if(value){
 				var _this = this;
 				// search for radio buttons with the same name that need to be unchecked
-				dojo.query("INPUT[type=radio][name^='"+this.name+"']", this.focusNode.form||dojo.doc).forEach( // can't use name= since dojo.query doesn't support [] in the name
+				dojo.query("INPUT[type=radio]", this.focusNode.form||dojo.doc).forEach( // can't use name= since dojo.query doesn't support [] in the name
 					function(inputNode){
 						if(inputNode.name == _this.name && inputNode != _this.focusNode && inputNode.form == _this.focusNode.form){
 							var widget = dijit.getEnclosingWidget(inputNode);
