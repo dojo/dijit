df9eb7beefe0448c917e5f661a050a52940a0169
Bill Keese <bill@dojotoolkit.org>
2009-02-18 08:22:01 +0000

    Continuation of [16667], use dojo.global to access child page.  Refs #7681.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16715 560b804f-0ae3-0310-86f3-f6aa0a117693

--- robotx.js
+++ robotx.js
@@ -8,7 +8,7 @@ var __updateDocument = doh.robot._updateDocument;
 dojo.mixin(doh.robot,{
 	_updateDocument: function(){
 		__updateDocument();
-		var win = (dojo.doc.parentWindow || dojo.doc.defaultView);
+		var win = dojo.global;
 		if(win["dijit"]){
 			dijit.registry = win.dijit.registry;
 		}
