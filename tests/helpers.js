231d3161e2052d6886a2aced4c64edcccd5b1f40
Bill Keese <bill@dojotoolkit.org>
2009-02-26 08:07:37 +0000

    Split menu tests into keyboard tests and mouse tests.   Only the mouse tests work on safari (because dijit doesn't support keyboard on safari).   Refs #7681.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16829 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/helpers.js
@@ -0,0 +1,13 @@
+// Helper methods for automated testing
+
+function isVisible(node){
+		if(node.domNode){ node = node.domNode; }
+		return (dojo.style(node, "display") != "none") &&
+				(dojo.style(node, "visibility") != "hidden");
+}
+
+function isHidden(node){
+		if(node.domNode){ node = node.domNode; }
+		return (dojo.style(node, "display") == "none") ||
+				(dojo.style(node, "visibility") == "hidden");
+}
