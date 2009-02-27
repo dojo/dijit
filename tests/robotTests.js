1f5cf70c3faedd4b09ce4d2e8a1ecab4b6a5d595
Bill Keese <bill@dojotoolkit.org>
2009-02-27 00:40:15 +0000

    Fix robotTests.js to run the Menu test, and also to run a11y only on browsers that support them.  Refs #7681.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16840 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/robotTests.js
+++ tests/robotTests.js
@@ -3,17 +3,27 @@ dojo.provide("dijit.tests.robotTests");
 try{
 if(dojo.isBrowser){
 	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g,"").replace(/^&/,"?");
+
+	// Safari doesn't support focus on nodes like <div>, so keyboard isn't supported there.
+	var test_a11y = dojo.isFF || dojo.isIE;
 	
 	// form widget tests
 	doh.registerUrl("dijit/tests/form/robot/test_ComboBox.html", dojo.moduleUrl("dijit","tests/form/robot/_autoComplete.html"+(userArgs+"&testWidget=dijit.form.ComboBox").replace(/^&/,"?")), 99999999);
 	doh.registerUrl("dijit/tests/form/robot/test_FilteringSelect.html", dojo.moduleUrl("dijit","tests/form/robot/_autoComplete.html"+(userArgs+"&testWidget=dijit.form.FilteringSelect").replace(/^&/,"?")), 99999999);
-	doh.registerUrl("dijit/tests/form/robot/test_Slider.html", dojo.moduleUrl("dijit","tests/form/robot/test_Slider.html"+userArgs), 99999999);
-	doh.registerUrl("dijit/tests/form/robot/test_Spinner.html", dojo.moduleUrl("dijit","tests/form/robot/test_Spinner.html"+userArgs), 99999999);
+	doh.registerUrl("dijit/tests/form/robot/Slider_mouse.html", dojo.moduleUrl("dijit","tests/form/robot/Slider_mouse.html"+userArgs), 99999999);
+	doh.registerUrl("dijit/tests/form/robot/Spinner_mouse.html", dojo.moduleUrl("dijit","tests/form/robot/Spinner_mouse.html"+userArgs), 99999999);
 	doh.registerUrl("dijit/tests/form/robot/test_validate.html", dojo.moduleUrl("dijit","tests/form/robot/test_validate.html"+userArgs), 99999999);
-	
+	if(test_a11y){
+		doh.registerUrl("dijit/tests/form/robot/Slider_a11y.html", dojo.moduleUrl("dijit","tests/form/robot/Slider_a11y.html"+userArgs), 99999999);
+		doh.registerUrl("dijit/tests/form/robot/Spinner_a11y.html", dojo.moduleUrl("dijit","tests/form/robot/Spinner_a11y.html"+userArgs), 99999999);
+	}
+
 	// top level widget tests
-	doh.registerUrl("dijit/tests/robot/test_Tree.html", dojo.moduleUrl("dijit","tests/robot/test_Tree.html"+userArgs), 99999999);
-	doh.registerUrl("dijit/tests/robot/test_Menu.html", dojo.moduleUrl("dijit","tests/robot/test_Menu.html"+userArgs), 99999999);
+	doh.registerUrl("dijit/tests/robot/Menu_mouse.html", dojo.moduleUrl("dijit","tests/robot/Menu_mouse.html"+userArgs), 99999999);
+	if(test_a11y){
+		doh.registerUrl("dijit/tests/robot/Tree_a11y.html", dojo.moduleUrl("dijit","tests/robot/Tree_a11y.html"+userArgs), 99999999);
+		doh.registerUrl("dijit/tests/robot/Menu_a11y.html", dojo.moduleUrl("dijit","tests/robot/Menu_a11y.html"+userArgs), 99999999);
+	}
 }
 }catch(e){
 	doh.debug(e);
