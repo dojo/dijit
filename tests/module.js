1ee03bc0ef6c152dcf6ab81b9f654a5b9f09b542
Bill Keese <bill@dojotoolkit.org>
2009-03-04 04:46:04 +0000

    Test case for nested StackContainer/TabContainer.  Refs #8799. Not working on IE since IE seems to be getting spurious resize events.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@16916 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -23,6 +23,7 @@ try{
 	// layout tests
 	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 30000);
 	doh.registerUrl("dijit.tests.layout.AccordionContainer", dojo.moduleUrl("dijit", "tests/layout/AccordionContainer.html"), 30000);
+	doh.registerUrl("dijit.tests.layout.StackContainer", dojo.moduleUrl("dijit", "tests/layout/nestedStack.html"));
 }catch(e){
 	doh.debug(e);
 }
