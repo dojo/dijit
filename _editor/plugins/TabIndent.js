4591d1f888bef4aca61a86aa587d817877d6abf0
Bill Keese <bill@dojotoolkit.org>
2009-04-07 10:50:49 +0000

    Remove dead code and cleanup code for TabIndent plugin. Refs #4607, #8734 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@17227 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/plugins/TabIndent.js
+++ _editor/plugins/TabIndent.js
@@ -22,46 +22,22 @@ dojo.declare("dijit._editor.plugins.TabIndent",
 
 		_initButton: function(){
 			// Override _Plugin._initButton() to setup listener on button click
-			this.inherited("_initButton", arguments);
-			this.connect(this.button, "onClick", this._tabIndent);
+			this.inherited(arguments);
 
-			// TODO: set initial checked state of button based on Editor.isTabIndent?
+			var e = this.editor;
+			this.connect(this.button, "onChange", function(val){
+				e.attr("isTabIndent", val);
+			});
+
+			// Set initial checked state of button based on Editor.isTabIndent
+			this.updateState();
 		},
 
 		updateState: function(){
 			// Overrides _Plugin.updateState().
 			// Since (apparently) Ctrl-m in the editor will switch tabIndent mode on/off, we need to react to that.
 
-			// TODO: can't all this code be replaced by a simple:
-			//		this.button.attr('checked', this.editor.isTabIndent);
-
-			var _e = this.editor;
-			var _c = this.command;
-			if(!_e){ return; }
-			if(!_e.isLoaded){ return; }
-			if(!_c.length){ return; }
-			if(this.button){
-				try{
-					var enabled = _e.isTabIndent;
-					if(typeof this.button.checked == 'boolean'){ 
-						this.button.attr('checked', enabled);
-					}
-				}catch(e){
-					console.debug(e);
-				}
-			}
-		},
-
-		_tabIndent: function(){
-			// summary:
-			//		Handler for checking/unchecking the toggle button.
-			//		Toggle the value for isTabIndent.
-
-			// TODO: probably better to connect to ToggleButton.onChange and then use the reported value, ex:
-			//		var e = this.editor;
-			//		this.connect(this.button, "onChange", function(val){ e.isTabIndent = val; })
-
-			this.editor.isTabIndent = !this.editor.isTabIndent;
+			this.button.attr('checked', this.editor.isTabIndent);
 		}
 	}
 );
