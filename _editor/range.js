7857aa1c50adb318fd1a2ee6f76076c9c96a2961
Heng Liu <liucougar@gmail.com>
2009-06-30 22:16:27 +0000

    refs #9478: accidentally committed something not intended adds \xA0 to regular expression: it is not included in \s
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@18498 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/range.js
+++ _editor/range.js
@@ -129,7 +129,7 @@ dijit.range.atBeginningOfContainer = function(/*DomNode*/container, /*DomNode*/n
 	var atBeginning = false;
 	var offsetAtBeginning = (offset == 0);
 	if(!offsetAtBeginning && node.nodeType==3){ //if this is a text node, check whether the left part is all space
-		if(/^\s+$/.test(node.nodeValue.substr(0,offset))){
+		if(/^[\s\xA0]+$/.test(node.nodeValue.substr(0,offset))){
 			offsetAtBeginning = true;
 		}
 	}
@@ -151,7 +151,7 @@ dijit.range.atEndOfContainer = function(/*DomNode*/container, /*DomNode*/node, /
 	var atEnd = false;
 	var offsetAtEnd = (offset == (node.length || node.childNodes.length));
 	if(!offsetAtEnd && node.nodeType==3){ //if this is a text node, check whether the right part is all space
-		if(/^\s+$/.test(node.nodeValue.substr(offset))){
+		if(/^[\s\xA0]+$/.test(node.nodeValue.substr(offset))){
 			offsetAtEnd = true;
 		}
 	}
@@ -253,7 +253,6 @@ if(!dijit.range._w3c){
 			return [startContainer, startOffset,endContainer, endOffset];
 		},
 		getEndPoint: function(range, end){
-			var originalparent=range.parentElement();
 			var atmrange = range.duplicate();
 			atmrange.collapse(!end);
 			var cmpstr = 'EndTo' + (end?'End':'Start');
@@ -324,8 +323,8 @@ if(!dijit.range._w3c){
 			//if at the end of startnode and we are dealing with start container, then
 			//move the startnode to nextSibling if it is a text node
 			//TODO: do this for end container?
-			if(!end && startOffset == (startnode.nodeType!=3 ? startnode.childNodes.length:startnode.length)){
-				var nextnode=teampatent.helper.range.nextNode(startnode,originalparent);
+			if(!end && startnode.nodeType==1 && startOffset == startnode.childNodes.length){
+				var nextnode=startnode.nextSibling;
 				if(nextnode && nextnode.nodeType==3){
 					startnode = nextnode;
 					startOffset = 0;
