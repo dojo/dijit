fe443e20f4963c6b643e85e59d0a92d332de4aad
Douglas Hays <doughays@dojotoolkit.org>
2009-06-30 16:14:54 +0000

    References #9410.  Added support for Opera 9.6 and Chrome 2.0.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@18482 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Textarea.js
+++ form/Textarea.js
@@ -32,9 +32,9 @@ dojo.declare(
 			newH += textarea.offsetHeight - textarea.clientHeight - ((dojo.isIE < 8 && this._strictMode)? dojo._getPadBorderExtents(textarea).h : 0);
 		}else if(dojo.isMoz){
 			newH += textarea.offsetHeight - textarea.clientHeight; // creates room for horizontal scrollbar
-		}else if(dojo.isSafari/*NOT isWebKit*/ >= 4){
+		}else if(dojo.isWebKit && !(dojo.isSafari < 4)){ // Safari 4.0 && Chrome
 			newH += dojo._getBorderExtents(textarea).h;
-		}else{
+		}else{ // Safari 3.x and Opera 9.6
 			newH += dojo._getPadBorderExtents(textarea).h;
 		}
 		return newH;
@@ -55,28 +55,28 @@ dojo.declare(
 		textarea.rows = (textarea.value.match(/\n/g) || []).length + 1;
 	},
 
+	_needsHelpShrinking: dojo.isMoz || dojo.isWebKit,
+
 	_onInput: function(){
 		// Override SimpleTextArea._onInput() to deal with height adjustment
 		this.inherited(arguments);
 		if(this._busyResizing){ return; }
 		this._busyResizing = true;
 		var textarea = this.textbox;
-		var newH = this._getHeight(textarea);
-		if(newH > 0){
-			newH = newH + "px";
+		if(textarea.scrollHeight){
+			var newH = this._getHeight(textarea) + "px";
 			if(textarea.style.height != newH){
 				textarea.style.maxHeight = textarea.style.height = newH;
 			}
-		}else if(dojo.isIE){ 
-			// hidden content of unknown size
-			// no call to _shrink so estimate the height here
-			this._estimateHeight(textarea);
-		}
-		if((dojo.isMoz || dojo.isSafari/*NOT isWebKit*/)){
-			if(this._setTimeoutHandle){
-				clearTimeout(this._setTimeoutHandle);
+			if(this._needsHelpShrinking){
+				if(this._setTimeoutHandle){
+					clearTimeout(this._setTimeoutHandle);
+				}
+				this._setTimeoutHandle = setTimeout(dojo.hitch(this, "_shrink"), 0); // try to collapse multiple shrinks into 1
 			}
-			this._setTimeoutHandle = setTimeout(dojo.hitch(this, "_shrink"), 0); // try to collapse multiple shrinks into 1
+		}else{
+			// hidden content of unknown size
+			this._estimateHeight(textarea);
 		}
 		this._busyResizing = false;
 	},
@@ -85,7 +85,7 @@ dojo.declare(
 	_shrink: function(){
 		// grow paddingBottom to see if scrollHeight shrinks (when it is unneccesarily big)
 		this._setTimeoutHandle = null;
-		if((dojo.isMoz || dojo.isSafari/*NOT isWebKit*/) && !this._busyResizing){
+		if(this._needsHelpShrinking && !this._busyResizing){
 			this._busyResizing = true;
 			var textarea = this.textbox;
 			var empty = false;
@@ -94,18 +94,21 @@ dojo.declare(
 				empty = true;
 			}
 			var scrollHeight = textarea.scrollHeight;
-			var oldPadding = textarea.style.paddingBottom;
-			var padding = dojo._getPadExtents(textarea);
-			textarea.style.paddingBottom = padding.h - padding.t + scrollHeight + "px";
-			textarea.scrollTop = 0;
-			var newH = this._getHeight(textarea) - scrollHeight + "px"; // scrollHeight is the added padding
 			if(!scrollHeight){
 				this._estimateHeight(textarea);
+			}else{
+				var oldPadding = textarea.style.paddingBottom;
+				var newPadding = dojo._getPadExtents(textarea);
+				newPadding = newPadding.h - newPadding.t;
+				textarea.style.paddingBottom = newPadding + 1 + "px"; // tweak padding to see if height can be reduced
+				var newH = this._getHeight(textarea) - 1 + "px"; // see if the height changed by the 1px added
+				if(textarea.style.maxHeight != newH){ // if can be reduced, so now try a big chunk
+					textarea.style.paddingBottom = newPadding + scrollHeight + "px";
+					textarea.scrollTop = 0;
+					textarea.style.maxHeight = this._getHeight(textarea) - scrollHeight + "px"; // scrollHeight is the added padding
+				}
+				textarea.style.paddingBottom = oldPadding;
 			}
-			else if(textarea.style.maxHeight != newH){
-				textarea.style.maxHeight = newH;
-			}
-			textarea.style.paddingBottom = oldPadding;
 			if(empty){
 				textarea.value = '';
 			}
@@ -117,7 +120,6 @@ dojo.declare(
 		// summary:
 		//		Resizes the textarea vertically (should be called after a style/value change)
 		this._onInput();
-		this._shrink();
 	},
 
 	_setValueAttr: function(){
@@ -131,7 +133,7 @@ dojo.declare(
 		dojo.style(this.textbox, { overflowY: 'hidden', overflowX: 'auto', boxSizing: 'border-box', MsBoxSizing: 'border-box', WebkitBoxSizing: 'border-box', MozBoxSizing: 'border-box' });
 		this.connect(this.textbox, "onscroll", this._onInput);
 		this.connect(this.textbox, "onresize", this._onInput);
-		this.connect(this.textbox, "onfocus", this.resize); // useful when a previous estimate was off a bit
+		this.connect(this.textbox, "onfocus", this._onInput); // useful when a previous estimate was off a bit
 		setTimeout(dojo.hitch(this, "resize"), 0);
 	}
 });
