ab1e89ad9f23a1b62246689ec758ae7177b57bc5
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-07-31 19:10:08 +0000

    Checking in cleanup to selection code.  Fixed Opera issue (it was being detected as IE and failing), plus API docs, etc. refs #9520  Tested using DOH Robot tests and manual tests on FF2, FF3.5, Safari 3.2.3, IE 7, IE 8.  IE 6 testing underway.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@19368 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/selection.js
+++ _editor/selection.js
@@ -7,8 +7,9 @@ dojo.provide("dijit._editor.selection");
 
 dojo.mixin(dijit._editor.selection, {
 	getType: function(){
-		// summary: Get the selection type (like dojo.doc.select.type in IE).
-		if(dojo.doc.selection){ //IE
+		// summary: 
+		//		Get the selection type (like dojo.doc.select.type in IE).
+		if(dojo.isIE){ 
 			return dojo.doc.selection.type.toLowerCase();
 		}else{
 			var stype = "text";
@@ -28,14 +29,14 @@ dojo.mixin(dijit._editor.selection, {
 					stype = "control";
 				}
 			}
-			return stype;
+			return stype; //String
 		}
 	},
 
 	getSelectedText: function(){
 		// summary:
 		//		Return the text (no html tags) included in the current selection or null if no text is selected
-		if(dojo.doc.selection){ //IE
+		if(dojo.isIE){ 
 			if(dijit._editor.selection.getType() == 'control'){
 				return null;
 			}
@@ -43,16 +44,16 @@ dojo.mixin(dijit._editor.selection, {
 		}else{
 			var selection = dojo.global.getSelection();
 			if(selection){
-				return selection.toString();
+				return selection.toString(); //String
 			}
 		}
-		return ''
+		return '';
 	},
 
 	getSelectedHtml: function(){
 		// summary:
-		//		Return the html of the current selection or null if unavailable
-		if(dojo.doc.selection){ //IE
+		//		Return the html text of the current selection or null if unavailable
+		if(dojo.isIE){ 
 			if(dijit._editor.selection.getType() == 'control'){
 				return null;
 			}
@@ -60,10 +61,16 @@ dojo.mixin(dijit._editor.selection, {
 		}else{
 			var selection = dojo.global.getSelection();
 			if(selection && selection.rangeCount){
-				var frag = selection.getRangeAt(0).cloneContents();
-				var div = dojo.doc.createElement("div");
-				div.appendChild(frag);
-				return div.innerHTML;
+				var i;
+				var html = "";
+				for(i = 0; i < selection.rangeCount; i++){
+					//Handle selections spanning ranges, such as Opera
+					var frag = selection.getRangeAt(i).cloneContents();
+					var div = dojo.doc.createElement("div");
+					div.appendChild(frag);
+					html += div.innerHTML;
+				}
+				return html; //String
 			}
 			return null;
 		}
@@ -75,7 +82,7 @@ dojo.mixin(dijit._editor.selection, {
 		//		a single element (object like and image or a table) is
 		//		selected.
 		if(dijit._editor.selection.getType() == "control"){
-			if(dojo.doc.selection){ //IE
+			if(dojo.isIE){ 
 				var range = dojo.doc.selection.createRange();
 				if(range && range.item){
 					return dojo.doc.selection.createRange().item(0);
@@ -95,19 +102,17 @@ dojo.mixin(dijit._editor.selection, {
 			var p = this.getSelectedElement();
 			if(p){ return p.parentNode; }
 		}else{
-			if(dojo.doc.selection){ //IE
-				var r=dojo.doc.selection.createRange();
+			if(dojo.isIE){ 
+				var r = dojo.doc.selection.createRange();
 				r.collapse(true);
 				return r.parentElement();
 			}else{
 				var selection = dojo.global.getSelection();
 				if(selection){
 					var node = selection.anchorNode;
-
 					while(node && (node.nodeType != 1)){ // not an element
 						node = node.parentNode;
 					}
-
 					return node;
 				}
 			}
@@ -119,25 +124,34 @@ dojo.mixin(dijit._editor.selection, {
 		// summary:
 		// 		Check whether current selection has a  parent element which is
 		// 		of type tagName (or one of the other specified tagName)
-		return this.getAncestorElement.apply(this, arguments) != null;
+		// tagName: String
+		//		The tag name to determine if it has an ancestor of.
+		return this.getAncestorElement.apply(this, arguments) != null; //Boolean
 	},
 
 	getAncestorElement: function(/*String*/tagName /* ... */){
 		// summary:
 		//		Return the parent element of the current selection which is of
 		//		type tagName (or one of the other specified tagName)
-
+		// tagName: String
+		//		The tag name to determine if it has an ancestor of.
 		var node = this.getSelectedElement() || this.getParentElement();
-		return this.getParentOfType(node, arguments);
+		return this.getParentOfType(node, arguments); //DOMNode
 	},
 
 	isTag: function(/*DomNode*/node, /*Array*/tags){
+		// summary:
+		//		Function to determine if a node is one of an array of tags.
+        // node: DOMNode
+		//		The node to inspect.
+		// tags: Array
+		//		An array of tag name strings to check to see if the node matches.
 		if(node && node.tagName){
 			var _nlc = node.tagName.toLowerCase();
 			for(var i=0; i<tags.length; i++){
 				var _tlc = String(tags[i]).toLowerCase();
 				if(_nlc == _tlc){
-					return _tlc;
+					return _tlc; //String
 				}
 			}
 		}
@@ -145,9 +159,15 @@ dojo.mixin(dijit._editor.selection, {
 	},
 
 	getParentOfType: function(/*DomNode*/node, /*Array*/tags){
+		// summary:
+		//		Function to locate a parent node that matches one of a set of tags
+		// node: DOMNode
+		//		The node to inspect.
+		// tags: Array
+		//		An array of tag name strings to check to see if the node matches.
 		while(node){
 			if(this.isTag(node, tags).length){
-				return node;
+				return node; //DOMNode
 			}
 			node = node.parentNode;
 		}
@@ -155,38 +175,42 @@ dojo.mixin(dijit._editor.selection, {
 	},
 
 	collapse: function(/*Boolean*/beginning) {
-		// summary: clear current selection
-	  if(window['getSelection']){
-	          var selection = dojo.global.getSelection();
-	          if(selection.removeAllRanges){ // Mozilla
-	                  if(beginning){
-	                          selection.collapseToStart();
-	                  }else{
-	                          selection.collapseToEnd();
-	                  }
-	          }else{ // Safari
-	                  // pulled from WebCore/ecma/kjs_window.cpp, line 2536
-	                   selection.collapse(beginning);
-	          }
-	  }else if(dojo.doc.selection){ // IE
-	          var range = dojo.doc.selection.createRange();
-	          range.collapse(beginning);
-	          range.select();
-	  }
+		// summary:
+		//		Function to collapse (clear), the current selection
+		// beginning: Boolean
+		//		Boolean to indicate whether to collapse the cursor to the beginning of the selection or end.
+		if(window.getSelection){
+			var selection = dojo.global.getSelection();
+			if(selection.removeAllRanges){ // Mozilla
+				if(beginning){
+					selection.collapseToStart();
+				}else{
+					selection.collapseToEnd();
+				}
+			}else{ // Safari
+				// pulled from WebCore/ecma/kjs_window.cpp, line 2536
+				selection.collapse(beginning);
+			}
+		}else if(dojo.isIE){ // IE
+			var range = dojo.doc.selection.createRange();
+			range.collapse(beginning);
+			range.select();
+		}
 	},
 
 	remove: function(){
-		// summary: delete current selection
-		var _s = dojo.doc.selection;
-		if(_s){ //IE
-			if(_s.type.toLowerCase() != "none"){
-				_s.clear();
+		// summary: 
+		//		Function to delete the currently selected content from the document.
+		var sel = dojo.doc.selection;
+		if(dojo.isIE){ 
+			if(sel.type.toLowerCase() != "none"){
+				sel.clear();
 			}
-			return _s;
+			return sel; //Selection
 		}else{
-			_s = dojo.global.getSelection();
-			_s.deleteFromDocument();
-			return _s;
+			sel = dojo.global.getSelection();
+			sel.deleteFromDocument();
+			return sel; //Selection
 		}
 	},
 
@@ -194,21 +218,38 @@ dojo.mixin(dijit._editor.selection, {
 		// summary:
 		//		clear previous selection and select the content of the node
 		//		(excluding the node itself)
-		var _window = dojo.global;
-		var _document = dojo.doc;
+		// element:	DOMNode
+		//		The element you wish to select the children content of.
+		// nochangefocus: Boolean
+		//		Boolean to indicate if the foxus should change or not.
+		var win = dojo.global;
+		var doc = dojo.doc;
+		var range;
 		element = dojo.byId(element);
-		if(_document.selection && dojo.body().createTextRange){ // IE
-			var range = element.ownerDocument.body.createTextRange();
+		if(doc.selection && dojo.isIE && dojo.body().createTextRange){ // IE
+			range = element.ownerDocument.body.createTextRange();
 			range.moveToElementText(element);
 			if(!nochangefocus){
 				try{
 					range.select(); // IE throws an exception here if the widget is hidden.  See #5439
 				}catch(e){ /* squelch */}
 			}
-		}else if(_window.getSelection){
-			var selection = _window.getSelection();
+		}else if(win.getSelection){
+			var selection = dojo.global.getSelection();
 			if(selection.setBaseAndExtent){ // Safari
 				selection.setBaseAndExtent(element, 0, element, element.innerText.length - 1);
+			}else if(dojo.isOpera){
+				//Opera's selectAllChildren doesn't seem to work right
+				//against <body> nodes and possibly others ... so
+				//we iuse the W3C range API 
+				if(selection.rangeCount){
+					range = selection.getRangeAt(0);
+				}else{
+					range = doc.createRange();
+				}
+				range.setStart(element, 0);
+				range.setEnd(element,element.nodeType==3?element.length:element.childNodes.length);
+				selection.addRange(range);
 			}else if(selection.selectAllChildren){ // Mozilla
 				selection.selectAllChildren(element);
 			}
@@ -218,9 +259,15 @@ dojo.mixin(dijit._editor.selection, {
 	selectElement: function(/*DomNode*/element,/*Boolean?*/nochangefocus){
 		// summary:
 		//		clear previous selection and select element (including all its children)
-		var range, _document = dojo.doc;
+		// element:  DOMNode
+		//		The element to select.
+		// nochangefocus: Boolean
+		//		Boolean indicating if the focus should be changed.  IE only.
+		var range;
+		var doc = dojo.doc;
+		var win = dojo.global;
 		element = dojo.byId(element);
-		if(_document.selection && dojo.body().createTextRange){ // IE
+		if(dojo.isIE && dojo.body().createTextRange){
 			try{
 				range = dojo.body().createControlRange();
 				range.addElement(element);
@@ -231,10 +278,17 @@ dojo.mixin(dijit._editor.selection, {
 				this.selectElementChildren(element,nochangefocus);
 			}
 		}else if(dojo.global.getSelection){
-			var selection = dojo.global.getSelection();
-			// FIXME: does this work on Safari?
+			var selection = win.getSelection();
+			range = doc.createRange();
 			if(selection.removeAllRanges){ // Mozilla
-				range = _document.createRange();
+				// FIXME: does this work on Safari? 
+				if(dojo.isOpera){
+					//Opera works if you use the current range on
+					//the selection if present.
+					if(selection.getRangeAt(0)){
+						range = selection.getRangeAt(0);
+					}
+				}
 				range.selectNode(element);
 				selection.removeAllRanges();
 				selection.addRange(range);
