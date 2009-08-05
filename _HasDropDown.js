2603194a50e858f338f200b287fdbcde999639fa
Bill Keese <bill@dojotoolkit.org>
2009-08-05 11:02:15 +0000

    Correctly account for page scroll when testing if the drop down menu has appeared over the drop down button, and the user has moused-up w/out moving off of the button.  (BTW this can be tested on test_Button.html by making the browser window very short and then scrolling down to the first row of buttons)
    Also switching from dojo.coords() to dojo.position().
    
    Refs #9356, #9648 !strict
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@19523 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _HasDropDown.js
+++ _HasDropDown.js
@@ -128,9 +128,9 @@ dojo.declare("dijit._HasDropDown",
 				// because it's so large.  In that case mouse-up shouldn't select a value from the menu.
 				// Find out if our target is somewhere in our dropdown widget,
 				// but not over our _buttonNode (the clickable node)
-				var c = dojo.coords(this._buttonNode)
-				if(!(e.pageX >= c.l && e.pageX <= c.l + c.w) || 
-					!(e.pageY >= c.t && e.pageY <= c.t + c.h)){
+				var c = dojo.position(this._buttonNode, true);
+				if(!(e.pageX >= c.x && e.pageX <= c.x + c.w) || 
+					!(e.pageY >= c.y && e.pageY <= c.y + c.h)){
 					var t = e.target;
 					while(t && !overMenu){
 						if(dojo.hasClass(t, "dijitPopup")){
