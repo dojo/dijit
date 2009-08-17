2017f411c3de2633d92895263d8ed45e1a15cb31
Bill Keese <bill@dojotoolkit.org>
2009-08-17 13:25:38 +0000

    InlineEditBox robot test and test file locale fix, courtesy of Youngho (CLA on file), thanks! Fixes #9703.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@19896 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -47,6 +47,7 @@ try{
 		doh.registerUrl("dijit.tests.robot.Menu_mouse", dojo.moduleUrl("dijit","tests/robot/Menu_mouse.html"+userArgs), 99999999);
 		doh.registerUrl("dijit.tests.robot.Dialog_mouse", dojo.moduleUrl("dijit","tests/robot/Dialog_mouse.html"+userArgs), 99999999);
 		doh.registerUrl("dijit.tests.robot.Tooltip_mouse", dojo.moduleUrl("dijit","tests/robot/Tooltip_mouse.html"+userArgs), 99999999);
+		doh.registerUrl("dijit.tests.robot.InlineEditBox_mouse", dojo.moduleUrl("dijit","tests/robot/InlineEditBox_mouse.html"+userArgs), 99999999);
 		if(test_a11y){
 			doh.registerUrl("dijit.tests.robot.Menu_a11y", dojo.moduleUrl("dijit","tests/robot/Menu_a11y.html"+userArgs), 99999999);
 			doh.registerUrl("dijit.tests.robot.Dialog_a11y", dojo.moduleUrl("dijit","tests/robot/Dialog_a11y.html"+userArgs), 99999999);
