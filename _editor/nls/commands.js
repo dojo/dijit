d79803a3dead08dff7385d3ab0fff0978f05b6c3
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-09-10 15:21:59 +0000

    Adding in simple 'Print' plugin to editor.  fixes #9777
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20072 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/nls/commands.js
+++ _editor/nls/commands.js
@@ -38,10 +38,11 @@
 	'fontSize': 'Font Size',
 	'fontName': 'Font Name',
 	'tabIndent': 'Tab Indent',
+	"fullScreen": "Toggle Full Screen",
+	"viewSource": "View HTML Source",
+	"print": "Print",
 	/* Error messages */
 	'systemShortcut': 'The "${0}" action is only available in your browser using a keyboard shortcut. Use ${1}.',
 	'ctrlKey':'ctrl+${0}',
-	'appleKey':'\u2318${0}', // "command" or open-apple key on Macintosh
-	"fullScreen": "Toggle Full Screen",
-	"viewSource": "View HTML Source"
+	'appleKey':'\u2318${0}' // "command" or open-apple key on Macintosh
 })
