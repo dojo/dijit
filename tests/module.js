0adc61c46de755dbb724236ac6d49634c7f169bc
Bill Keese <bill@dojotoolkit.org>
2009-09-16 13:02:55 +0000

    Keyboard is more-or-less working for safari and chrome.   Enabling the keyboard tests for all browsers.   Refs #9506, #8987.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20113 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/module.js
+++ tests/module.js
@@ -3,10 +3,7 @@ dojo.provide("dijit.tests.module");
 try{
 	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g,"").replace(/^&/,"?");
 
-	// Safari 3 doesn't support focus on nodes like <div>, so keyboard isn't supported there...
-	// Safari 4 almost works, but has problems with shift-tab (#8987) and ESC (#9506).
-	// Enable tests when those bugs are fixed.
-	var test_a11y = dojo.isFF || dojo.isIE;
+	var test_a11y = true;
 	
 	var test_robot = true;
 
