1a21e853491a728114b10b9d55c0c657e95b41a1
Bill Keese <bill@dojotoolkit.org>
2009-10-07 11:45:47 +0000

    #8987 and #9506 are fixed, so the a11y tests can run on webkit now too.   Actually they were running already from dijit/tests/module.js.  Refs #9377.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20444 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/editor/module.js
+++ tests/editor/module.js
@@ -3,30 +3,21 @@ dojo.provide("dijit.tests.editor.module");
 try{
 	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g,"").replace(/^&/,"?");
 
-	// Safari 3 doesn't support focus on nodes like <div>, so keyboard isn't supported there...
-	// Safari 4 almost works, but has problems with shift-tab (#8987) and ESC (#9506).
-	// Enable tests when those bugs are fixed.
-	var test_a11y = dojo.isFF || dojo.isIE;
-	
-	var test_robot = true;
+	// Base editor functionality
+	doh.registerUrl("dijit.tests.editor.robot.Editor_mouse", dojo.moduleUrl("dijit","tests/editor/robot/Editor_mouse.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.Editor_a11y", dojo.moduleUrl("dijit","tests/editor/robot/Editor_a11y.html"+userArgs), 99999999);
 
-	// editor tests
-	if(test_robot){
-		doh.registerUrl("dijit.tests.editor.robot.Editor_mouse", dojo.moduleUrl("dijit","tests/editor/robot/Editor_mouse.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.EnterKeyHandling", dojo.moduleUrl("dijit","tests/editor/robot/EnterKeyHandling.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.FullScreen", dojo.moduleUrl("dijit","tests/editor/robot/Editor_FullScreen.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.ViewSource", dojo.moduleUrl("dijit","tests/editor/robot/Editor_ViewSource.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.NewPage", dojo.moduleUrl("dijit","tests/editor/robot/Editor_NewPage.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.LinkDialog", dojo.moduleUrl("dijit","tests/editor/robot/Editor_LinkDialog.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.editor.robot.FontChoice", dojo.moduleUrl("dijit","tests/editor/robot/Editor_FontChoice.html"+userArgs), 99999999);
-		if(!dojo.isWebKit){
-			// The back button on webkit is URL for the browser itself, restarting the entire test suite,
-			// rather than just for the iframe holding the test file (BackForwardState.html and BackForwardStateHelper.html)
-			doh.registerUrl("dijit.tests.editor.robot.BackForwardState", dojo.moduleUrl("dijit","tests/editor/robot/BackForwardState.html"+userArgs), 99999999);
-		}
-		if(test_a11y){
-			doh.registerUrl("dijit.tests.editor.robot.Editor_a11y", dojo.moduleUrl("dijit","tests/editor/robot/Editor_a11y.html"+userArgs), 99999999);
-		}
+	// Plugins
+	doh.registerUrl("dijit.tests.editor.robot.EnterKeyHandling", dojo.moduleUrl("dijit","tests/editor/robot/EnterKeyHandling.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.FullScreen", dojo.moduleUrl("dijit","tests/editor/robot/Editor_FullScreen.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.ViewSource", dojo.moduleUrl("dijit","tests/editor/robot/Editor_ViewSource.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.NewPage", dojo.moduleUrl("dijit","tests/editor/robot/Editor_NewPage.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.LinkDialog", dojo.moduleUrl("dijit","tests/editor/robot/Editor_LinkDialog.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.editor.robot.FontChoice", dojo.moduleUrl("dijit","tests/editor/robot/Editor_FontChoice.html"+userArgs), 99999999);
+	if(!dojo.isWebKit){
+		// The back button on webkit is URL for the browser itself, restarting the entire test suite,
+		// rather than just for the iframe holding the test file (BackForwardState.html and BackForwardStateHelper.html)
+		doh.registerUrl("dijit.tests.editor.robot.BackForwardState", dojo.moduleUrl("dijit","tests/editor/robot/BackForwardState.html"+userArgs), 99999999);
 	}
 
 }catch(e){
