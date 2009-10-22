039cc7b8e1cffdcbcd6d46a3ebcf824605a53dcb
Bill Keese <bill@dojotoolkit.org>
2009-10-22 09:13:16 +0000

    Rename InlineEditBox_mouse.html since it now contains keyboard tests too, fix a comment position in that file, and fix general-modules.js file.  Refs #10016.
    Still getting failures on IE8 for TooltipDialog_a11y.html and InlineEditBox.html.
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20632 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/general-module.js
+++ tests/general-module.js
@@ -13,8 +13,9 @@ try{
 	doh.registerUrl("dijit.tests.robot.Tooltip_mouse", dojo.moduleUrl("dijit","tests/robot/Tooltip_mouse.html"+userArgs), 99999999);
 	doh.registerUrl("dijit.tests.robot.Tooltip_a11y", dojo.moduleUrl("dijit","tests/robot/Tooltip_a11y.html"+userArgs), 99999999);
 
-	doh.registerUrl("dijit.tests.robot.InlineEditBox_mouse", dojo.moduleUrl("dijit","tests/robot/TooltipDialog_a11y.html"+userArgs), 99999999);
-	doh.registerUrl("dijit.tests.robot.InlineEditBox_mouse", dojo.moduleUrl("dijit","tests/robot/InlineEditBox_mouse.html"+userArgs), 99999999);
+	doh.registerUrl("dijit.tests.robot.TooltipDialog_a11y", dojo.moduleUrl("dijit","tests/robot/TooltipDialog_a11y.html"+userArgs), 99999999);
+
+	doh.registerUrl("dijit.tests.robot.InlineEditBox", dojo.moduleUrl("dijit","tests/robot/InlineEditBox.html"+userArgs), 99999999);
 
 }catch(e){
 	doh.debug(e);
