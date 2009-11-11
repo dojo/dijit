76fa20e9424d8033221eb7590af088a2f3f3195e
Douglas Hays <doughays@dojotoolkit.org>
2009-11-11 23:58:54 +0000

    Refs #7681.  Slow down automated tests to work better with VM environments.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20779 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/layout/module.js
+++ tests/layout/module.js
@@ -1,11 +1,11 @@
 dojo.provide("dijit.tests.layout.module");
 
 try{
-	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 30000);
-	doh.registerUrl("dijit.tests.layout.ContentPaneLayout", dojo.moduleUrl("dijit", "tests/layout/ContentPaneLayout.html"), 30000);
-	doh.registerUrl("dijit.tests.layout.AccordionContainer", dojo.moduleUrl("dijit", "tests/layout/AccordionContainer.html"), 30000);
-	doh.registerUrl("dijit.tests.layout.TabContainer", dojo.moduleUrl("dijit", "tests/layout/TabContainer.html"), 30000);
-	doh.registerUrl("dijit.tests.layout.StackContainer", dojo.moduleUrl("dijit", "tests/layout/nestedStack.html"));
+	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 300000);
+	doh.registerUrl("dijit.tests.layout.ContentPaneLayout", dojo.moduleUrl("dijit", "tests/layout/ContentPaneLayout.html"), 300000);
+	doh.registerUrl("dijit.tests.layout.AccordionContainer", dojo.moduleUrl("dijit", "tests/layout/AccordionContainer.html"), 300000);
+	doh.registerUrl("dijit.tests.layout.TabContainer", dojo.moduleUrl("dijit", "tests/layout/TabContainer.html"), 300000);
+	doh.registerUrl("dijit.tests.layout.StackContainer", dojo.moduleUrl("dijit", "tests/layout/nestedStack.html"), 300000);
 }catch(e){
 	doh.debug(e);
 }
