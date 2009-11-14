57c9ac8fb8bff3453c7f0b4823995692fd7fc3ed
Bill Keese <bill@dojotoolkit.org>
2009-11-14 15:51:22 +0000

    remove reference to test_robot, it's no longer being used.  refs #7681.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@20808 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/infrastructure-module.js
+++ tests/infrastructure-module.js
@@ -6,10 +6,9 @@ try{
 	doh.registerUrl("dijit.tests._Widget-attr", dojo.moduleUrl("dijit", "tests/_Widget-attr.html"));
 	doh.registerUrl("dijit.tests._Widget-subscribe", dojo.moduleUrl("dijit", "tests/_Widget-subscribe.html"));
 	doh.registerUrl("dijit.tests.Widget-placeAt", dojo.moduleUrl("dijit", "tests/Widget-placeAt.html"));
-	if(test_robot){
-		doh.registerUrl("dijit.tests.robot._Widget-deferredConnect", dojo.moduleUrl("dijit","tests/robot/_Widget-deferredConnect.html"), 99999999);
-		doh.registerUrl("dijit.tests.robot._Widget-ondijitclick_mouse", dojo.moduleUrl("dijit","tests/robot/_Widget-ondijitclick_mouse.html"), 99999999);
-	}
+	doh.registerUrl("dijit.tests.robot._Widget-deferredConnect", dojo.moduleUrl("dijit","tests/robot/_Widget-deferredConnect.html"), 99999999);
+	doh.registerUrl("dijit.tests.robot._Widget-ondijitclick_mouse", dojo.moduleUrl("dijit","tests/robot/_Widget-ondijitclick_mouse.html"), 99999999);
+
 	doh.registerUrl("dijit.tests.robot._Widget-ondijitclick_a11y", dojo.moduleUrl("dijit","tests/robot/_Widget-ondijitclick_a11y.html"), 99999999);
 
 	// _Templated and other base classes
