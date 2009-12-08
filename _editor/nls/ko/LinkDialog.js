9ada7a5519fa7e57510681e1f440b71044eeba5c
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-12-08 21:06:28 +0000

    Adding translations to 1.4.  refs #10413
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dijit@21008 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/nls/ko/LinkDialog.js
+++ _editor/nls/ko/LinkDialog.js
@@ -3,5 +3,10 @@
 	insertImageTitle: "이미지 등록 정보",
 	url: "URL:",
 	text: "설명:",
-	set: "설정"
+	target: "대상:",    
+	set: "설정",
+	currentWindow: "현재 창",
+	parentWindow: "부모 창",
+	topWindow: "최 상위 창",
+	newWindow: "새 창"    
 })
