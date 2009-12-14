9e8465d774aa7d59a27bf6a33da1607d52348319
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-12-14 19:25:17 +0000

    Fix for potential reflected XSS in some dojox test php files.  Only affects php servers and deployments of dojo that include the tests.  Fix is escapement of user input to avoid script tag insertion.  refs #10478
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@21040 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/layout/getResponse.php
+++ tests/layout/getResponse.php
@@ -40,16 +40,16 @@
 				echo "It was the year of Our Lord one thousand seven hundred and seventy- five. Spiritual revelations were conceded to England at that favoured period, as at this. Mrs. Southcott had recently attained her five-and- twentieth blessed birthday, of whom a prophetic private in the Life Guards had heralded the sublime appearance by announcing that arrangements were made for the swallowing up of London and Westminster. Even the Cock-lane ghost had been laid only a round dozen of years, after rapping out its messages, as the spirits of this very year last past (supernaturally deficient in originality) rapped out theirs. Mere messages in the earthly order of events had lately come to the English Crown and People, from a congress of British subjects in America:";
 				break;
 			default:
-				echo "unknown messId:{$_GET['messId']}";
+				echo "unknown messId:". htmlentities($_GET['messId']);
 		}
 	}
 
 	if(isset($_GET['bounceGetStr']) &&  $_GET['bounceGetStr']){
-		echo "<div id='bouncedGetStr'>{$_SERVER["QUERY_STRING"]}</div>";
+		echo "<div id='bouncedGetStr'>".htmlentities($_SERVER["QUERY_STRING"])."</div>";
 	}
 
 	if(isset($_GET['message']) && $_GET['message']){
-		echo $_GET['message'];
+		echo htmlentities($_GET['message']);
 	}
 
 	usleep($delay);
