0397cb0254f0ed053d2252086c7d1c76a196a9e7
Adam Peller <github@peller.org>
2010-01-14 17:15:50 +0000

    Accidentally applied translations to 1.3 branch instead of 1.4.  Backing out all but tabIndent.  Refs #10506, Refs #10470
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.3/dijit@21158 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/nls/it/commands.js
+++ _editor/nls/it/commands.js
@@ -38,10 +38,6 @@
 	'fontSize': 'Dimensione carattere',
 	'fontName': 'Nome carattere',
 	'tabIndent': 'Rientro Tab',
-	'fullScreen': 'Schermo Intero',
-	'viewSource': 'Visualizza Sorgente HTML',
-	'print': 'Stampa',
-	'newPage': 'Nuova Pagina',
 	/* Error messages */
 	'systemShortcut': 'Azione "${0}" disponibile sul proprio browser solo mediante i tasti di scelta rapida della tastiera. Utilizzare ${1}.'
 })
