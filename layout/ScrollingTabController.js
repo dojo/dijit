877ee7a160968dc1dcc90e6f5bf9c743228e561f
Bill Keese <bill@dojotoolkit.org>
2010-02-11 09:47:18 +0000

    Making scrolling tabs work in RTL mode on webkit (see themeTester.html?dir=rtl) and IE8/quirks mode (see themeTesterQuirk.html?dir=rtl).
    Webkit has the same scrollLeft meaning (in RTL mode) as IE6/7.  Wonder if webkit will change to match the spec in the future.
    
    Fixes #10742, refs #9004 !strict
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@21344 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/ScrollingTabController.js
+++ layout/ScrollingTabController.js
@@ -230,7 +230,7 @@ dojo.declare("dijit.layout.ScrollingTabController",
 		//		Returns the current scroll of the tabs where 0 means
 		//		"scrolled all the way to the left" and some positive number, based on #
 		//		of pixels of possible scroll (ex: 1000) means "scrolled all the way to the right"
-		var sl = (this.isLeftToRight() || dojo.isIE < 8) ? this.scrollNode.scrollLeft :
+		var sl = (this.isLeftToRight() || dojo.isIE < 8 || dojo.isQuirks || dojo.isWebKit) ? this.scrollNode.scrollLeft :
 				dojo.style(this.containerNode, "width") - dojo.style(this.scrollNode, "width")
 					 + (dojo.isIE == 8 ? -1 : 1) * this.scrollNode.scrollLeft;
 		return sl;
@@ -244,7 +244,7 @@ dojo.declare("dijit.layout.ScrollingTabController",
 		//		to achieve that scroll.
 		//
 		//		This method is to adjust for RTL funniness in various browsers and versions.
-		if(this.isLeftToRight() || dojo.isIE < 8){
+		if(this.isLeftToRight() || dojo.isIE < 8 || dojo.isQuirks || dojo.isWebKit){
 			return val;
 		}else{
 			var maxScroll = dojo.style(this.containerNode, "width") - dojo.style(this.scrollNode, "width");
