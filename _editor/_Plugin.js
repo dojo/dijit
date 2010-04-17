e612f06ef8d576e9cc6d362d0dfa3054057e46b4
Bill Keese <bill@dojotoolkit.org>
2010-04-17 11:56:13 +0000

    Remove unneeded dojo.require(), fixes #10924 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@21873 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/_Plugin.js
+++ _editor/_Plugin.js
@@ -1,6 +1,5 @@
 dojo.provide("dijit._editor._Plugin");
 dojo.require("dijit._Widget");
-dojo.require("dijit.Editor");
 dojo.require("dijit.form.Button");
 
 dojo.declare("dijit._editor._Plugin", null, {
