0bfcd9692b7c69a3cc1489bd1a4d85d8d7a7c5f5
Douglas Hays <doughays@dojotoolkit.org>
2010-06-05 12:52:15 +0000

    References #10527 [22286].  The menu is still 1px offscreen causing the _autoComplete_mouse.html test to file.  Adjust isVisible method to allow the border to be offscreen as long as the content is visible.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@22297 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/helpers.js
+++ tests/helpers.js
@@ -4,14 +4,14 @@ function isVisible(node){
 		if(node.domNode){ node = node.domNode; }
 		return (dojo.style(node, "display") != "none") &&
 				(dojo.style(node, "visibility") != "hidden") &&
-				(dojo.position(node).y >= 0);
+				(dojo.position(node).y + (dojo._getBorderExtents(node).t || 0) >= 0); // border check is for claro prone to shifting the border offscreen
 }
 
 function isHidden(node){
 		if(node.domNode){ node = node.domNode; }
 		return (dojo.style(node, "display") == "none") ||
 				(dojo.style(node, "visibility") == "hidden") ||
-				(dojo.position(node).y < 0);
+				(dojo.position(node).y < 0); // + dojo.position(node).h ??
 }
 
 function innerText(node){
