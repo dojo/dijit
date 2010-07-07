4a725b4633d59d7bde78f30fd1c0bc573113d916
Bill Keese <bill@dojotoolkit.org>
2010-07-07 03:42:23 +0000

    Textarea robot test, fixes #10366.   It's not practical to tests things like cut/paste through the browser's native context menu, or increasing/decreasing the page zoom, but at least this tests some stuff.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@22448 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/form/module.js
+++ tests/form/module.js
@@ -30,6 +30,8 @@ try{
 
 	doh.registerUrl("dijit.tests.form.CheckBox", dojo.moduleUrl("dijit", "tests/form/test_CheckBox.html"), 99999999);
 
+	doh.registerUrl("dijit.tests.form.robot.Textarea", dojo.moduleUrl("dijit","tests/form/robot/Textarea.html"+userArgs), 99999999);
+
 	doh.registerUrl("dijit.tests.form.robot.validationMessages", dojo.moduleUrl("dijit","tests/form/robot/validationMessages.html"+userArgs), 99999999);
 
 	doh.registerUrl("dijit.tests.form.TextBox_sizes.tundra.ltr", dojo.moduleUrl("dijit", "tests/form/TextBox_sizes.html?theme=tundra&dir=ltr"), 99999999);
