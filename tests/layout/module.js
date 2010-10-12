ba1f3c16db35d673bf4b8011d95f3625376387c3
Douglas Hays <doughays@dojotoolkit.org>
2010-10-12 14:19:06 +0000

    Refs #11488.  Proxy commit for ksv (CCLA).  Added new automated dijit/layout tests.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@22996 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/layout/module.js
+++ tests/layout/module.js
@@ -3,13 +3,20 @@ dojo.provide("dijit.tests.layout.module");
 try{
 	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 99999999);
 	doh.registerUrl("dijit.tests.layout.ContentPaneLayout", dojo.moduleUrl("dijit", "tests/layout/ContentPaneLayout.html"), 99999999);
+	doh.registerUrl("dijit.tests.layout.robot.ContentPane_prog", dojo.moduleUrl("dijit", "tests/layout/robot/ContentPane_prog.html"), 99999999); 
 
-	doh.registerUrl("dijit.tests.layout.StackContainer", dojo.moduleUrl("dijit", "tests/layout/nestedStack.html"), 99999999); 
+	doh.registerUrl("dijit.tests.layout.LayoutContainer_a11y", dojo.moduleUrl("dijit", "tests/layout/robot/LayoutContainer_a11y.html"), 99999999);
+
+	doh.registerUrl("dijit.tests.layout.NestedStackContainer", dojo.moduleUrl("dijit", "tests/layout/nestedStack.html"), 99999999); 
+	doh.registerUrl("dijit.tests.layout.StackContainer_mouse", dojo.moduleUrl("dijit", "tests/layout/robot/StackContainer_mouse.html"), 99999999); 
 
 	doh.registerUrl("dijit.tests.layout.TabContainer", dojo.moduleUrl("dijit", "tests/layout/TabContainer.html"), 99999999);
 	doh.registerUrl("dijit.tests.layout.robot.TabContainer_a11y", dojo.moduleUrl("dijit","tests/layout/robot/TabContainer_a11y.html"), 99999999);
 	doh.registerUrl("dijit.tests.layout.robot.TabContainer_mouse", dojo.moduleUrl("dijit","tests/layout/robot/TabContainer_mouse.html"), 99999999);
-
+	doh.registerUrl("dijit.tests.layout.robot.TabContainer_noLayout", dojo.moduleUrl("dijit","tests/layout/robot/TabContainer_noLayout.html"), 99999999);
+	doh.registerUrl("dijit.tests.layout.robot.TabContainer_prog", dojo.moduleUrl("dijit","tests/layout/robot/TabContainer_prog.html"), 99999999);
+	doh.registerUrl("dijit.tests.layout.robot.TabContainer_remote", dojo.moduleUrl("dijit","tests/layout/robot/TabContainer_remote.html"), 99999999);
+	
 	doh.registerUrl("dijit.tests.layout.AccordionContainer", dojo.moduleUrl("dijit", "tests/layout/AccordionContainer.html"), 99999999);
 	doh.registerUrl("dijit.tests.layout.robot.AccordionContainer_a11y", dojo.moduleUrl("dijit","tests/layout/robot/AccordionContainer_a11y.html"), 99999999);
 	doh.registerUrl("dijit.tests.layout.robot.AccordionContainer_mouse", dojo.moduleUrl("dijit","tests/layout/robot/AccordionContainer_mouse.html"), 99999999);
