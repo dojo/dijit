a149cfaae3c49c47ebbe0f0605c0f89564067616
Douglas Hays <doughays@dojotoolkit.org>
2010-10-15 13:56:47 +0000

    Refs #11488.  Reorganize dijit.Tree tests under tree/ directory and remove redundant test files. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@23016 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/tree/module.js
+++ tests/tree/module.js
@@ -4,18 +4,18 @@ try{
 	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g,"").replace(/^&/,"?"),
 		test_robot = true;
 
-	doh.registerUrl("dijit.tests.Tree", dojo.moduleUrl("dijit", "tests/Tree.html"), 99999999);
+	doh.registerUrl("dijit.tests.tree.Tree", dojo.moduleUrl("dijit", "tests/tree/Tree.html"), 99999999);
 	
 	// comment out until #11621 is fixed
-	//doh.registerUrl("dijit.tests.Tree_with_JRS", dojo.moduleUrl("dijit", "tests/Tree_with_JRS.html"), 99999999);
+	//doh.registerUrl("dijit.tests.tree.Tree_with_JRS", dojo.moduleUrl("dijit", "tests/tree/Tree_with_JRS.html"), 99999999);
 
 	if(test_robot){
-		doh.registerUrl("dijit.tests.robot.Tree_a11y", dojo.moduleUrl("dijit","tests/robot/Tree_a11y.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.robot.Tree_DnD", dojo.moduleUrl("dijit","tests/robot/Tree_dnd.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.robot.Tree_selector", dojo.moduleUrl("dijit","tests/robot/Tree_selector.html"+userArgs), 99999999);
-		doh.registerUrl("dijit.tests.robot.Tree_selector_only", 
-			dojo.moduleUrl("dijit","tests/robot/Tree_selector.html?controller=selector&"+userArgs.substr(1)), 99999999);
-		doh.registerUrl("dijit.tests.robot.Tree_DnD_multiParent", dojo.moduleUrl("dijit","tests/robot/Tree_dnd_multiParent.html"+userArgs), 99999999);
+		doh.registerUrl("dijit.tests.tree.robot.Tree_a11y", dojo.moduleUrl("dijit","tests/tree/robot/Tree_a11y.html"+userArgs), 99999999);
+		doh.registerUrl("dijit.tests.tree.robot.Tree_DnD", dojo.moduleUrl("dijit","tests/tree/robot/Tree_dnd.html"+userArgs), 99999999);
+		doh.registerUrl("dijit.tests.tree.robot.Tree_selector", dojo.moduleUrl("dijit","tests/tree/robot/Tree_selector.html"+userArgs), 99999999);
+		doh.registerUrl("dijit.tests.tree.robot.Tree_selector_only", 
+			dojo.moduleUrl("dijit","tests/tree/robot/Tree_selector.html?controller=selector&"+userArgs.substr(1)), 99999999);
+		doh.registerUrl("dijit.tests.tree/robot.Tree_DnD_multiParent", dojo.moduleUrl("dijit","tests/tree/robot/Tree_dnd_multiParent.html"+userArgs), 99999999);
 	}
 }catch(e){
 	doh.debug(e);
