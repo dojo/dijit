6c3bb67789f224fd7ca77bf9f850a6eeb9af5431
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2010-11-05 14:14:39 +0000

    Followup patch for Saf 4 issues with Editor startup.  Saf 4 bug.  \!strict refs #11744
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@23105 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/RichText.js
+++ _editor/RichText.js
@@ -466,10 +466,13 @@ dojo.declare("dijit._editor.RichText", [dijit._Widget, dijit._CssStateMixin], {
 		ifr.setAttribute('src', s);
 		this.editingArea.appendChild(ifr);
 
-		if(dojo.isSafari <= 4 && !dojo.isMac){ 
-			// Safari 4 and earlier on windows only seems to always append iframe with src=about:blank
-			// So we have to set it again.
-			setTimeout(function(){ifr.setAttribute('src', s);},0);
+		if(dojo.isSafari <= 4){
+			var src = ifr.getAttribute("src");
+			if(!src || src.indexOf("javascript") == -1){ 
+				// Safari 4 and earlier sometimes act oddly
+				// So we have to set it again.
+				setTimeout(function(){ifr.setAttribute('src', s);},0);
+			}
 		}
 
 		// TODO: this is a guess at the default line-height, kinda works
