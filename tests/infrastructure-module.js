3e0290e64decca00b61f296ee3ed8a310413bf91
Bill Keese <bill@dojotoolkit.org>
2010-12-27 00:16:08 +0000

    Test changes needed after removing _top and _left attributes, plus fixing filename, refs #11030.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@23446 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/infrastructure-module.js
+++ tests/infrastructure-module.js
@@ -5,7 +5,7 @@ try{
 	doh.registerUrl("dijit.tests._Widget-lifecycle", dojo.moduleUrl("dijit", "tests/_Widget-lifecycle.html"), 999999);
 	doh.registerUrl("dijit.tests._Widget-attr", dojo.moduleUrl("dijit", "tests/_Widget-attr.html"), 999999);
 	doh.registerUrl("dijit.tests._Widget-subscribe", dojo.moduleUrl("dijit", "tests/_Widget-subscribe.html"), 999999);
-	doh.registerUrl("dijit.tests.Widget-placeAt", dojo.moduleUrl("dijit", "tests/Widget-placeAt.html"), 999999);
+	doh.registerUrl("dijit.tests._Widget-placeAt", dojo.moduleUrl("dijit", "tests/_Widget-placeAt.html"), 999999);
 	doh.registerUrl("dijit.tests.robot._Widget-deferredConnect", dojo.moduleUrl("dijit","tests/robot/_Widget-deferredConnect.html"), 999999);
 	doh.registerUrl("dijit.tests.robot._Widget-ondijitclick_mouse", dojo.moduleUrl("dijit","tests/robot/_Widget-ondijitclick_mouse.html"), 999999);
 
@@ -21,7 +21,7 @@ try{
 	doh.registerUrl("dijit.tests.Declaration_1.x", dojo.moduleUrl("dijit","tests/test_Declaration_1.x.html"), 999999);
 
 	// Miscellaneous
-	doh.registerUrl("dijit.tests.NodeList-instantiate", dojo.moduleUrl("dijit","tests/NodeList-instantiate.html"+userArgs), 999999);
+	doh.registerUrl("dijit.tests.NodeList-instantiate", dojo.moduleUrl("dijit","tests/NodeList-instantiate.html"), 999999);
 }catch(e){
 	doh.debug(e);
 }
