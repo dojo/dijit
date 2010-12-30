698969081bcfe18bf827c041d99e106b82668022
Bill Keese <bill@dojotoolkit.org>
2010-12-30 08:05:48 +0000

    Convert test_!ContentPane.html to load under AMD, to detect parse failures, and add to run as part of layout test suite, refs #11869.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@23462 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/layout/module.js
+++ tests/layout/module.js
@@ -2,6 +2,7 @@ dojo.provide("dijit.tests.layout.module");
 
 try{
 	doh.registerUrl("dijit.tests.layout.ContentPane", dojo.moduleUrl("dijit", "tests/layout/ContentPane.html"), 999999);
+	doh.registerUrl("dijit.tests.layout.test_ContentPane", dojo.moduleUrl("dijit", "tests/layout/test_ContentPane.html"), 999999);
 	doh.registerUrl("dijit.tests.layout.ContentPaneLayout", dojo.moduleUrl("dijit", "tests/layout/ContentPaneLayout.html"), 999999);
 
 	doh.registerUrl("dijit.tests.layout.robot.GUI", dojo.moduleUrl("dijit","tests/layout/robot/GUI.html"), 999999);
