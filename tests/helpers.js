37ec7fbc3448a1f6cd7061a7a7a0eac3d4ea0db4
Bill Keese <bill@dojotoolkit.org>
2011-02-24 12:13:50 +0000

    Convert most of !TabContainer a11y test to listen for focus events, rather than using timers.  Also cleaned up tabbing.
    Refs #10279 !strict.
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@23881 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,6 +1,8 @@
 // Helper methods for automated testing
 
-function isVisible(node){
+function isVisible(/*dijit._Widget || DomNode*/ node){
+	// summary:
+	//		Return true if node/widget is visible
 	var p;
 	if(node.domNode){ node = node.domNode; }
 	return (dojo.style(node, "display") != "none") &&
@@ -8,7 +10,9 @@ function isVisible(node){
 		(p = dojo.position(node), p.y + p.h >= 0 && p.x + p.w >= 0 && p.h && p.w);
 }
 
-function isHidden(node){
+function isHidden(/*dijit._Widget || DomNode*/ node){
+	// summary:
+	//		Return true if node/widget is hidden
 	var p;
 	if(node.domNode){ node = node.domNode; }
 	return (dojo.style(node, "display") == "none") ||
@@ -16,7 +20,9 @@ function isHidden(node){
 		(p = dojo.position(node), p.y + p.h < 0 || p.x + p.w < 0 || p.h <= 0 || p.w <= 0);
 }
 
-function innerText(node){
+function innerText(/*DomNode*/ node){
+	// summary:
+	//		Browser portable function to get the innerText of specified DOMNode
 	return node.textContent || node.innerText || "";
 }
 
@@ -54,4 +60,15 @@ function tabOrder(/*DomNode?*/ root){
 		return a.tabIndex != b.tabIndex ? a.tabIndex - b.tabIndex : a.pos - b.pos;
 	});
 	return dojo.map(elems, function(elem){ return elem.elem; });
+}
+
+
+function onFocus(func){
+	// summary:
+	//		On the next change of focus, and after widget has had time to react to focus event,
+	//		call func(node) with the newly focused node
+	var handle = dojo.subscribe("focusNode", function(node){
+		dojo.unsubscribe(handle);
+		setTimeout(function(){ func(node); }, 0);
+	});
 }
