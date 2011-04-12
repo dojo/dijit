89d9032c002923433f8bff6aed1f91ba7b36d4ec
Bill Keese <bill@dojotoolkit.org>
2011-04-12 18:07:09 +0000

    Don't display tooltip for left/right/menu buttons that merely matches the label/icon of the button.   Fixes #11160 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@24287 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/Button.js
+++ form/Button.js
@@ -83,7 +83,7 @@ dojo.declare("dijit.form.Button",
 		//		If the label is hidden (showLabel=false) then and no title has
 		//		been specified, then label is also set as title attribute of icon.
 		this.inherited(arguments);
-		if(this.showLabel == false && !this.params.title){
+		if(this.showLabel == false && !("title" in this.params)){
 			this.titleNode.title = dojo.trim(this.containerNode.innerText || this.containerNode.textContent || '');
 		}
 	},
