6d914751c194c910237444a8669284c82f9c205b
Bill Keese <bill@dojotoolkit.org>
2011-05-01 11:14:21 +0000

    fix summary comment, refs #12672
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@24573 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _base/sniff.js
+++ _base/sniff.js
@@ -1,16 +1,12 @@
-define(["dojo", "..", "dojo/uacss"], function(dojo, dijit) {
+define(["dojo", "..", "dojo/uacss"], function(dojo, dijit){
 	// module:
 	//		dijit/_base/sniff
 	// summary:
-	//		TODOC
+	//		Applies pre-set CSS classes to the top-level HTML node, see
+	//		`dojo.uacss` for details.
+	//
+	//		Simply doing a require on this module will
+	//		establish this CSS.  Modified version of Morris' CSS hack.
 
-// summary:
-//		Applies pre-set CSS classes to the top-level HTML node, see
-//		`dojo.uacss` for details.
-//
-//		Simply doing a require on this module will
-//		establish this CSS.  Modified version of Morris' CSS hack.
-
-
-return dijit;
+	return dijit;
 });
