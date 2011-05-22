5df84d56c775b60200d168a97afe82fff95f0250
Bill Keese <bill@dojotoolkit.org>
2011-05-22 10:47:39 +0000

    fixing CSS syntax errors, removing needless "px" in "0px" expressions, fixing file paths, refs #13036 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@25122 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/_ComboBoxMenu.js
+++ form/_ComboBoxMenu.js
@@ -27,7 +27,7 @@ define([
 		// tags:
 		//		private
 
-		templateString: "<div class='dijitReset dijitMenu' dojoAttachPoint='containerNode' style='overflow: \"auto\"; overflow-x: \"hidden\";'>"
+		templateString: "<div class='dijitReset dijitMenu' dojoAttachPoint='containerNode' style='overflow: auto; overflow-x: hidden;'>"
 				+"<div class='dijitMenuItem dijitMenuPreviousButton' dojoAttachPoint='previousButton' role='option'></div>"
 				+"<div class='dijitMenuItem dijitMenuNextButton' dojoAttachPoint='nextButton' role='option'></div>"
 				+"</div>",
