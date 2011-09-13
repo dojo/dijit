416d2c360bf4b1e31d41c4f7eb653fa96b16accf
Bill Keese <bill@dojotoolkit.org>
2011-09-13 01:56:42 +0000

    remove obsolete comment, refs #11621
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@26547 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/tree/module.js
+++ tests/tree/module.js
@@ -6,8 +6,6 @@ try{
 
 	doh.registerUrl("dijit.tests.tree.CustomLabel", dojo.moduleUrl("dijit", "tests/tree/CustomLabel.html"), 999999);
 	doh.registerUrl("dijit.tests.tree.Tree", dojo.moduleUrl("dijit", "tests/tree/Tree.html"), 999999);
-	
-	// comment out until #11621 is fixed
 	doh.registerUrl("dijit.tests.tree.Tree_with_JRS", dojo.moduleUrl("dijit", "tests/tree/Tree_with_JRS.html"), 999999);
 
 	if(test_robot){
@@ -15,7 +13,7 @@ try{
 		doh.registerUrl("dijit.tests.tree.robot.Tree_DnD", dojo.moduleUrl("dijit","tests/tree/robot/Tree_dnd.html"+userArgs), 999999);
 		doh.registerUrl("dijit.tests.tree.robot.Tree_selector", dojo.moduleUrl("dijit","tests/tree/robot/Tree_selector.html"+userArgs), 999999);
 		doh.registerUrl("dijit.tests.tree.robot.Tree_selector_only",
-			dojo.moduleUrl("dijit","tests/tree/robot/Tree_selector.html?controller=selector&"+userArgs.substr(1)), 999999);
+		dojo.moduleUrl("dijit","tests/tree/robot/Tree_selector.html?controller=selector&"+userArgs.substr(1)), 999999);
 		doh.registerUrl("dijit.tests.tree/robot.Tree_DnD_multiParent", dojo.moduleUrl("dijit","tests/tree/robot/Tree_dnd_multiParent.html"+userArgs), 999999);
 		doh.registerUrl("dijit.tests.tree.robot.Tree_v1", dojo.moduleUrl("dijit","tests/tree/robot/Tree_v1.html"+userArgs), 999999);
 	}
