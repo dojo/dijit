6f56cc7ee97080b43581acfd97545222a842ac54
Bill Keese <bill@dojotoolkit.org>
2011-11-18 10:40:02 +0000

    Test case for new dijit/place module (based on old dijit/_base/place module).   Refs #12728.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@27050 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/infrastructure-module.js
+++ tests/infrastructure-module.js
@@ -1,6 +1,9 @@
 dojo.provide("dijit.tests.infrastructure-module");
 
 try{
+	// Utility methods (previously in dijit/_base)
+	doh.registerUrl("dijit.tests.place", dojo.moduleUrl("dijit", "tests/place.html"), 999999);
+
 	// _Widget
 	doh.registerUrl("dijit.tests._Widget-lifecycle", dojo.moduleUrl("dijit", "tests/_Widget-lifecycle.html"), 999999);
 	doh.registerUrl("dijit.tests._Widget-attr", dojo.moduleUrl("dijit", "tests/_Widget-attr.html"), 999999);
