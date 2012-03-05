274deb3b4a6377ec0903585085a6e87854bb4067
Bill Keese <bill@dojotoolkit.org>
2012-03-05 09:11:30 +0000

    Avoid 10s timeout for the whole test file.  Refs #12957 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@28087 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_BidiSupport/tree/module.js
+++ tests/_BidiSupport/tree/module.js
@@ -2,7 +2,7 @@ dojo.provide("dijit.tests._BidiSupport.tree.module");
 
 try{
 
-	doh.registerUrl("dijit.tests._BidiSupport.tree.ProgrammaticTree.html", dojo.moduleUrl("dijit","tests/_BidiSupport/tree/ProgrammaticTree.html"));
+	doh.registerUrl("dijit.tests._BidiSupport.tree.ProgrammaticTree.html", dojo.moduleUrl("dijit","tests/_BidiSupport/tree/ProgrammaticTree.html"), 999999);
 
 }catch(e){
 
