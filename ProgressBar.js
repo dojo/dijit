051e8619c1b40092a54cc79825c4c7841c21b7fb
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2012-05-01 23:46:10 +0000

    Do not assume existence of this.params in ProgressBar#postMixInProperties; fixes #15293
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@28491 560b804f-0ae3-0310-86f3-f6aa0a117693

--- ProgressBar.js
+++ ProgressBar.js
@@ -79,7 +79,7 @@ return declare("dijit.ProgressBar", [_Widget, _TemplatedMixin], {
 		this.inherited(arguments);
 
 		// Back-compat for when constructor specifies indeterminate or progress, rather than value.   Remove for 2.0.
-		if(!("value" in this.params)){
+		if(!(this.params && "value" in this.params)){
 			this.value = this.indeterminate ? Infinity : this.progress;
 		}
 	},
