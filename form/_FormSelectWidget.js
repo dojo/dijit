27f26a986afe6e498e99f5d6fa193b99f2f03315
Douglas Hays <doughays@dojotoolkit.org>
2012-06-30 11:47:13 +0000

    Fixes #15572.  SelectedOption suffix class assumed a single baseClass. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@29093 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/_FormSelectWidget.js
+++ form/_FormSelectWidget.js
@@ -479,7 +479,7 @@ var _FormSelectWidget = declare("dijit.form._FormSelectWidget", _FormValueWidget
 				var isSelected = array.some(val, function(v){
 					return child.option && (v === child.option.value);
 				});
-				domClass.toggle(child.domNode, this.baseClass + "SelectedOption", isSelected);
+				domClass.toggle(child.domNode, this.baseClass.replace(/\s+|$/g, "SelectedOption "), isSelected);
 				child.domNode.setAttribute("aria-selected", isSelected ? "true" : "false");
 			}, this);
 		}
