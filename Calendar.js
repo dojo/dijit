7507d874f81c1081008beaae1010ceec42a65a83
Bill Keese <bill@dojotoolkit.org>
2012-07-14 08:17:41 +0000

    Fix selecting 'Elul' and 'Sivan' months from drop down in Calendar with Hebrew date class, fixes #15125 !strict.  Patch from Helena Halperin (IBM, CCLA), thanks!
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@29224 560b804f-0ae3-0310-86f3-f6aa0a117693

--- Calendar.js
+++ Calendar.js
@@ -83,9 +83,14 @@ define([
 			//		protected
 
 			// move to selected month, bounding by the number of days in the month
-			// (ex: dec 31 --> jan 28, not jan 31)
-			this._setCurrentFocusAttr(this.dateModule.add(this.currentFocus, "month",
-				newMonth - this.currentFocus.getMonth()));
+			// (ex: jan 31 --> feb 28, not feb 31)
+			var date  = new this.dateClassObj(this.currentFocus);
+			date.setDate(1);
+			date.setMonth(newMonth);
+			var daysInMonth = this.dateModule.getDaysInMonth(date);
+			var currentDate = this.currentFocus.getDate();
+			date.setDate(Math.min(currentDate, daysInMonth));
+			this._setCurrentFocusAttr(date);
 		},
 
 		_onDayMouseOver: function(/*Event*/ evt){
