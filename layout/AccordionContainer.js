fb8270dd0bc47ec915e19a2772403d56e0149d51
Bill Keese <bill@dojotoolkit.org>
2012-10-31 07:54:59 +0000

    Add role=tabpanel and aria-labelledby to wrappers of !AccordionContainer child widgets,  refs #16242 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@29898 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/AccordionContainer.js
+++ layout/AccordionContainer.js
@@ -190,7 +190,9 @@ define([
 
 			// and then the actual content widget (changing it from prior-sibling to last-child),
 			// wrapped by a <div class=dijitAccordionChildWrapper>
-			this.containerNode = domConstruct.place("<div class='dijitAccordionChildWrapper' style='display:none'>", this.domNode);
+			this.containerNode = domConstruct.place("<div class='dijitAccordionChildWrapper' role='tabpanel' style='display:none'>", this.domNode);
+			this.containerNode.setAttribute("aria-labelledby", this.button.id);
+
 			domConstruct.place(this.contentWidget.domNode, this.containerNode);
 		},
 
@@ -275,7 +277,7 @@ define([
 		buildRendering: function(){
 			this.inherited(arguments);
 			this.domNode.style.overflow = "hidden";		// TODO: put this in dijit.css
-			this.domNode.setAttribute("role", "tablist");	// TODO: put this in template
+			this.domNode.setAttribute("role", "tablist");
 		},
 
 		startup: function(){
@@ -306,7 +308,7 @@ define([
 			// get cumulative height of all the unselected title bars
 			var totalCollapsedHeight = 0;
 			array.forEach(this.getChildren(), function(child){
-	            if(child != openPane){
+				if(child != openPane){
 					// Using domGeometry.getMarginSize() rather than domGeometry.position() since claro has 1px bottom margin
 					// to separate accordion panes.  Not sure that works perfectly, it's probably putting a 1px
 					// margin below the bottom pane (even though we don't want one).
