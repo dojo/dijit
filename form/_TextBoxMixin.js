15651985c5b7f8e5d384c8291d9d340825225ac5
Douglas Hays <doughays@dojotoolkit.org>
2012-11-13 13:01:58 +0000

    Fixes #16311.  Merge TextBox::_onInput into _TextBoxMixin::_onInput so that intermediateChanges is visible to mobile/TextBox.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@29941 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/_TextBoxMixin.js
+++ form/_TextBoxMixin.js
@@ -206,6 +206,11 @@ var _TextBoxMixin = declare("dijit.form._TextBoxMixin", null, {
 		this.applyTextDir(this.focusNode);
 
 		this._processInput(evt);
+
+		if(this.intermediateChanges){
+			// allow the key to post to the widget input box
+			this.defer(function(){ this._handleOnChange(this.get('value'), false); });
+		}
 	},
 
 	_processInput: function(/*Event*/ evt){
