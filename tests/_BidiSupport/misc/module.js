1d556b989dc8c2e6d8adf968f5826eba9030e600
Bill Keese <bill@dojotoolkit.org>
2012-11-20 11:24:09 +0000

    Usually the Tooltip test takes 6 or 7 seconds, but on a bad day it can exceed 10, so increase timeout.    Refs #13884 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@29983 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/_BidiSupport/misc/module.js
+++ tests/_BidiSupport/misc/module.js
@@ -2,9 +2,9 @@ dojo.provide("dijit.tests._BidiSupport.misc.module");
 
 try{
 
-	doh.registerUrl("dijit.tests._BidiSupport.misc.Tooltip.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/Tooltip.html"));
-	doh.registerUrl("dijit.tests._BidiSupport.misc.Dialog.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/Dialog.html"));
-	doh.registerUrl("dijit.tests._BidiSupport.misc.MenuItem.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/MenuItem.html"));
+	doh.registerUrl("dijit.tests._BidiSupport.misc.Tooltip.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/Tooltip.html"), 999999);
+	doh.registerUrl("dijit.tests._BidiSupport.misc.Dialog.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/Dialog.html"), 999999);
+	doh.registerUrl("dijit.tests._BidiSupport.misc.MenuItem.html", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/MenuItem.html"), 999999);
 	doh.registerUrl("dijit.tests._BidiSupport.layout.TitlePane", dojo.moduleUrl("dijit","tests/_BidiSupport/misc/TitlePane.html"), 999999);
 
 }catch(e){
