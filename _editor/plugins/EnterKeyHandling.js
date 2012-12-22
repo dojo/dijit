81c66e694b6321bbde01559c43f3f3f71d8445ac
Bill Keese <bill@dojotoolkit.org>
2012-12-22 18:36:09 +0000

    Backport [28185] !EnterKeyHandling IE9 fixes to 1.6 branch, fixes #14949 on 1.6 branch !strict.   For some reason though I had to setup the onpaste listener on editor.document.body rather than on editor.document, like 1.7 does.
    Backport !EnterKeyHandling IE10 code and test fixes from [30091] to 1.6 branch.  Fixes #16127 on 1.6 branch !strict
    
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.6/dijit@30248 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _editor/plugins/EnterKeyHandling.js
+++ _editor/plugins/EnterKeyHandling.js
@@ -92,17 +92,30 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 			// we need to enable customUndo, if not already enabled.
 			this.editor.customUndo = true;
 				editor.onLoadDeferred.addCallback(dojo.hitch(this,function(d){
-				this.connect(editor.document, "onkeypress", function(e){
-					if(e.charOrCode == dojo.keys.ENTER){
-						// Just do it manually.  The handleEnterKey has a shift mode that
-						// Always acts like <br>, so just use it.
-						var ne = dojo.mixin({},e);
-						ne.shiftKey = true;
-						if(!this.handleEnterKey(ne)){
-							dojo.stopEvent(e);
+					this.connect(editor.document, "onkeypress", function(e){
+						if(e.charOrCode == dojo.keys.ENTER){
+							// Just do it manually.  The handleEnterKey has a shift mode that
+							// Always acts like <br>, so just use it.
+							var ne = dojo.mixin({},e);
+							ne.shiftKey = true;
+							if(!this.handleEnterKey(ne)){
+								dojo.stopEvent(e);
+							}
 						}
+					});
+					if(dojo.isIE >= 9){
+						this.connect(editor.document.body, "onpaste", function(e){
+							setTimeout(dojo.hitch(this, function(){
+								// Use the old range/selection code to kick IE 9 into updating
+								// its range by moving it back, then forward, one 'character'.
+								var r = this.editor.document.selection.createRange();
+								r.move('character',-1);
+								r.select();
+								r.move('character',1);
+								r.select();
+							}),0);
+						});
 					}
-				});
 					return d;
 				}));
 		}else if(this.blockNodeForEnter){
@@ -129,7 +142,7 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 					var block = dojo.withGlobal(this.editor.window, 'getAncestorElement', dijit._editor.selection, [this.blockNodeForEnter]);
 					if(block){
 						block.innerHTML=this.bogusHtmlContent;
-						if(dojo.isIE){
+						if(dojo.isIE <= 9){
 							// move to the start by moving backwards one char
 							var r = this.editor.document.selection.createRange();
 							r.move('character',-1);
