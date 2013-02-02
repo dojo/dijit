36d9959b35c301fdfd6f612cb3ce81e5efbbe80e
Bill Keese <bill@dojotoolkit.org>
2013-02-02 01:17:34 +0000

    since the parser will eventually only return a promise, start preparing for it now, refs #14591 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@30523 560b804f-0ae3-0310-86f3-f6aa0a117693

--- _WidgetsInTemplateMixin.js
+++ _WidgetsInTemplateMixin.js
@@ -1,8 +1,9 @@
 define([
 	"dojo/_base/array", // array.forEach
 	"dojo/_base/declare", // declare
+	"dojo/_base/lang",	// lang.hitch()
 	"dojo/parser" // parser.parse
-], function(array, declare, parser){
+], function(array, declare, lang, parser){
 
 	// module:
 	//		dijit/_WidgetsInTemplateMixin
@@ -35,27 +36,28 @@ define([
 				// Before copying over content, instantiate widgets in template
 				var node = this.domNode;
 
-				var cw = (this._startupWidgets = parser.parse(node, {
-					noStart: !this._earlyTemplatedStartup,
+				parser.parse(node, {
+					noStart: true,
 					template: true,
 					inherited: {dir: this.dir, lang: this.lang, textDir: this.textDir},
 					propsThis: this,	// so data-dojo-props of widgets in the template can reference "this" to refer to me
 					contextRequire: this.contextRequire,
 					scope: "dojo"	// even in multi-version mode templates use dojoType/data-dojo-type
+				}).then(lang.hitch(this, function(widgets){
+					this._startupWidgets = widgets;
+
+					// _WidgetBase::destroy() will destroy any supporting widgets under this.domNode.
+					// If we wanted to, we could call this.own() on anything in this._startupWidgets that was moved outside
+					// of this.domNode (like Dialog, which is moved to <body>).
+					this._attachTemplateNodes(widgets, function(n,p){
+						return n[p];
+					});
 				}));
 
-				if(!cw.isFulfilled()){
+				if(!this._startupWidgets){
 					throw new Error(this.declaredClass + ": parser returned unfilled promise (probably waiting for module auto-load), " +
 						"unsupported by _WidgetsInTemplateMixin.   Must pre-load all supporting widgets before instantiation.");
 				}
-
-				// _WidgetBase::destroy() will destroy any supporting widgets under this.domNode.
-				// If we wanted to, we could call this.own() on anything in this._startupWidgets that was moved outside
-				// of this.domNode (like Dialog, which is moved to <body>).
-
-				this._attachTemplateNodes(cw, function(n,p){
-					return n[p];
-				});
 			}
 		},
 
