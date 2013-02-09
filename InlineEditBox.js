e0b01b5294ba386842a3be821b9e2ea08ab03d4d
Douglas Hays <doughays@dojotoolkit.org>
2013-02-09 12:24:44 +0000

    Fixes #16525.  Change InlineEditBox to set the editing widget's value after onLoadDeferred so that Select works.  Sync 1.8 Select with trunk.  Undeprecate removeOption(String).!strict
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.8/dijit@30568 560b804f-0ae3-0310-86f3-f6aa0a117693

--- InlineEditBox.js
+++ InlineEditBox.js
@@ -95,7 +95,8 @@ define([
 				lang: this.lang,
 				textDir: this.textDir
 			});
-			editorParams[ "displayedValue" in Cls.prototype ? "displayedValue" : "value"] = this.value;
+			// set the value in onLoadDeferred instead so the widget has time to finish initializing
+			//editorParams[("displayedValue" in Cls.prototype || "_setDisplayedValueAttr" in Cls.prototype) ? "displayedValue" : "value"] = this.value;
 			this.editWidget = new Cls(editorParams, this.editorPlaceholder);
 
 			if(this.inlineEditBox.autoSave){
@@ -149,7 +150,7 @@ define([
 			// summary:
 			//		Return the [display] value of the edit widget
 			var ew = this.editWidget;
-			return String(ew.get("displayedValue" in ew ? "displayedValue" : "value"));
+			return String(ew.get(("displayedValue" in ew || "_getDisplayedValueAttr" in ew) ? "displayedValue" : "value"));
 		},
 
 		_onKeyPress: function(e){
@@ -457,10 +458,7 @@ define([
 			// save some display node values that can be restored later
 			this._savedTabIndex = domAttr.get(this.displayNode, "tabIndex") || "0";
 
-			if(this.wrapperWidget){
-				var ew = this.wrapperWidget.editWidget;
-				ew.set("displayedValue" in ew ? "displayedValue" : "value", this.value);
-			}else{
+			if(!this.wrapperWidget){
 				// Placeholder for edit widget
 				// Put place holder (and eventually editWidget) before the display node so that it's positioned correctly
 				// when Calendar dropdown appears, which happens automatically on focus.
@@ -506,8 +504,14 @@ define([
 			// or immediately if there is no onLoadDeferred Deferred,
 			// replace the display widget with edit widget, leaving them both displayed for a brief time so that
 			// focus can be shifted without incident.
-			when(ww.editWidget.onLoadDeferred, lang.hitch(ww, function(){
+			var ew = ww.editWidget;
+			var self = this;
+			when(ew.onLoadDeferred, lang.hitch(ww, function(){
+				// set value again in case the edit widget's value is just now valid
+				ew.set(("displayedValue" in ew || "_setDisplayedValueAttr" in ew) ? "displayedValue" : "value", self.value);
 				this.defer(function(){ // defer needed so that the change of focus doesn't happen on mousedown which also sets focus
+					// the saveButton should start out disabled in most cases but the above set could have fired onChange
+					ww.saveButton.set("disabled", "intermediateChanges" in ew);
 					this.focus(); // both nodes are showing, so we can switch focus safely
 					this._resetValue = this.getValue();
 				});
