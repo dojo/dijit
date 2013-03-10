b5d47e7feb2cc61651962d6d9d108df4cadf358b
Bill Keese <bill@dojotoolkit.org>
2013-03-10 02:41:44 +0000

    Update dijit/a11yclick to handle keyboard equivalent of ctrl-click, shift-click, etc.   Fixes #16822 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@30806 560b804f-0ae3-0310-86f3-f6aa0a117693

--- a11yclick.js
+++ a11yclick.js
@@ -11,8 +11,7 @@ define([
 		// Test if this keyboard event should be tracked as the start (if keydown) or end (if keyup) of a click event.
 		// Only track for nodes marked to be tracked, and not for buttons or inputs since they handle keyboard click
 		// natively.
-		if((e.keyCode === keys.ENTER || e.keyCode === keys.SPACE) &&
-				!e.ctrlKey && !e.shiftKey && !e.altKey && !e.metaKey && !/input|button/i.test(e.target.nodeName)){
+		if((e.keyCode === keys.ENTER || e.keyCode === keys.SPACE) && !/input|button/i.test(e.target.nodeName)){
 
 			// Test if a node or its ancestor has been marked with the dojoClick property to indicate special processing
 			for(var node = e.target; node; node = node.parentNode){
@@ -24,7 +23,7 @@ define([
 	var lastKeyDownNode;
 
 	on(document, "keydown", function(e){
-		//console.log(this.id + ": onkeydown, e.target = ", e.target, ", lastKeyDownNode was ", lastKeyDownNode, ", equality is ", (e.target === lastKeyDownNode));
+		//console.log("a11yclick: onkeydown, e.target = ", e.target, ", lastKeyDownNode was ", lastKeyDownNode, ", equality is ", (e.target === lastKeyDownNode));
 		if(clickKey(e)){
 			// needed on IE for when focus changes between keydown and keyup - otherwise dropdown menus do not work
 			lastKeyDownNode = e.target;
@@ -32,18 +31,24 @@ define([
 			// Prevent viewport scrolling on space key in IE<9.
 			// (Reproducible on test_Button.html on any of the first dijit/form/Button examples)
 			e.preventDefault();
+		}else{
+			lastKeyDownNode = null;
 		}
 	});
 
 	on(document, "keyup", function(e){
-		//console.log(this.id + ": onkeyup, e.target = ", e.target, ", lastKeyDownNode was ", lastKeyDownNode, ", equality is ", (e.target === lastKeyDownNode));
+		//console.log("a11yclick: onkeyup, e.target = ", e.target, ", lastKeyDownNode was ", lastKeyDownNode, ", equality is ", (e.target === lastKeyDownNode));
 		if(clickKey(e) && e.target == lastKeyDownNode){	// === breaks greasemonkey
 			//need reset here or have problems in FF when focus returns to trigger element after closing popup/alert
 			lastKeyDownNode = null;
 
 			on.emit(e.target, "click", {
 				cancelable: true,
-				bubbles: true
+				bubbles: true,
+				ctrlKey: e.ctrlKey,
+				shiftKey: e.shiftKey,
+				metaKey: e.metaKey,
+				altKey: e.altKey
 			});
 		}
 	});
