4220fb73d310e4a57f999b3b3a0404131c8dddbc
Bill Keese <bill@dojotoolkit.org>
2013-03-21 00:26:54 +0000

    doc fix, refs #8606 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@30948 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/_ListBase.js
+++ form/_ListBase.js
@@ -9,7 +9,7 @@ define([
 
 	return declare("dijit.form._ListBase", null, {
 		// summary:
-		//		Focus-less menu to handle UI events consistently
+		//		Focus-less menu to handle UI events consistently.
 		//		Abstract methods that must be defined externally:
 		//
 		//		- onSelect: item is active (mousedown but not yet mouseup, or keyboard arrow selected but no Enter)
