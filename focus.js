9de5f5c7f5df191596bab83a18e8871eeba1158e
Bill Keese <bill@dojotoolkit.org>
2013-03-21 13:09:37 +0000

    Change focus manager to use dojo/on rather than addEventListener() and attachEvent() on older IE's. Also increase timeout of _justMouseDowned handler to 13ms.
    Finally (not a cherry-pick), increased the _clearActiveWidgetsTimer timer to 100ms,
    needed to avoid error in TooltipDialog_a11y.html test where pressing the space bar
    on the InlineEditBox (inside a TooltipDialog) causes the TooltipDialog to close on IE8.
    
    Includes squash commit of:
    ---
    Change focus manager to use dojo/on rather than addEventListener() and attachEvent() on older IE's.
    This makes the behavior consistent across browsers (as to listening on capture phase vs. bubbling phase),
    and as a side effect fixes #13341 !strict.
    
    Also fixes various tests on IE11, such as tests/Dialog.html.
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@30957 560b804f-0ae3-0310-86f3-f6aa0a117693
    (cherry picked from commit 9965406f7184e8cee4467efc893ca5d85637b2e1)
    
    Conflicts:
    	focus.js
    
    ---
    Fix regression from [30957]: evt.target is the normalized way to reference the event target, not evt.srcElement.  Fixes exception on firefox.  Refs #13341 !strict.
    
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@30994 560b804f-0ae3-0310-86f3-f6aa0a117693
    (cherry picked from commit a42b8ed0fc2c297ad36931a2fa028fd000bae266)
    
    ---
    Increase the timeout of `_justMouseDowned` handler to 13 ms.
    
    Fixes #17668
    
    (cherry picked from commit 7cb2066cebba23d9e08af5f2e0210203bde3436a)

--- focus.js
+++ focus.js
@@ -7,25 +7,21 @@ define([
 	"dojo/Evented",
 	"dojo/_base/lang", // lang.hitch
 	"dojo/on",
-	"dojo/ready",
+	"dojo/domReady",
 	"dojo/_base/sniff", // has("ie")
 	"dojo/Stateful",
-	"dojo/_base/unload", // unload.addOnWindowUnload
 	"dojo/_base/window", // win.body
 	"dojo/window", // winUtils.get
 	"./a11y",	// a11y.isTabNavigable
 	"./registry",	// registry.byId
-	"."		// to set dijit.focus
-], function(aspect, declare, dom, domAttr, domConstruct, Evented, lang, on, ready, has, Stateful, unload, win, winUtils,
+	"./main"		// to set dijit.focus
+], function(aspect, declare, dom, domAttr, domConstruct, Evented, lang, on, domReady, has, Stateful, win, winUtils,
 			a11y, registry, dijit){
 
 	// module:
 	//		dijit/focus
-	// summary:
-	//		Returns a singleton that tracks the currently focused node, and which widgets are currently "active".
 
-/*=====
-	dijit.focus = {
+	var FocusManager = declare([Stateful, Evented], {
 		// summary:
 		//		Tracks the currently focused node, and which widgets are currently "active".
 		//		Access via require(["dijit/focus"], function(focus){ ... }).
@@ -45,46 +41,7 @@ define([
 		//		Currently focused item on screen
 		curNode: null,
 
-		// activeStack: dijit._Widget[]
-		//		List of currently active widgets (focused widget and it's ancestors)
-		activeStack: [],
-
-		registerIframe: function(iframe){
-			// summary:
-			//		Registers listeners on the specified iframe so that any click
-			//		or focus event on that iframe (or anything in it) is reported
-			//		as a focus/click event on the <iframe> itself.
-			// description:
-			//		Currently only used by editor.
-			// returns:
-			//		Handle with remove() method to deregister.
-		},
-
-		registerWin: function(targetWindow, effectiveNode){
-			// summary:
-			//		Registers listeners on the specified window (either the main
-			//		window or an iframe's window) to detect when the user has clicked somewhere
-			//		or focused somewhere.
-			// description:
-			//		Users should call registerIframe() instead of this method.
-			// targetWindow: Window?
-			//		If specified this is the window associated with the iframe,
-			//		i.e. iframe.contentWindow.
-			// effectiveNode: DOMNode?
-			//		If specified, report any focus events inside targetWindow as
-			//		an event on effectiveNode, rather than on evt.target.
-			// returns:
-			//		Handle with remove() method to deregister.
-		}
-	};
-=====*/
-
-	var FocusManager = declare([Stateful, Evented], {
-		// curNode: DomNode
-		//		Currently focused item on screen
-		curNode: null,
-
-		// activeStack: dijit._Widget[]
+		// activeStack: dijit/_WidgetBase[]
 		//		List of currently active widgets (focused widget and it's ancestors)
 		activeStack: [],
 
@@ -106,7 +63,7 @@ define([
 			// summary:
 			//		Registers listeners on the specified iframe so that any click
 			//		or focus event on that iframe (or anything in it) is reported
-			//		as a focus/click event on the <iframe> itself.
+			//		as a focus/click event on the `<iframe>` itself.
 			// description:
 			//		Currently only used by editor.
 			// returns:
@@ -132,92 +89,82 @@ define([
 
 			// TODO: make this function private in 2.0; Editor/users should call registerIframe(),
 
-			var _this = this;
-			var mousedownListener = function(evt){
-				_this._justMouseDowned = true;
-				setTimeout(function(){ _this._justMouseDowned = false; }, 0);
-
-				// workaround weird IE bug where the click is on an orphaned node
-				// (first time clicking a Select/DropDownButton inside a TooltipDialog)
-				if(has("ie") && evt && evt.srcElement && evt.srcElement.parentNode == null){
-					return;
-				}
-
-				_this._onTouchNode(effectiveNode || evt.target || evt.srcElement, "mouse");
-			};
-
 			// Listen for blur and focus events on targetWindow's document.
-			// IIRC, I'm using attachEvent() rather than dojo.connect() because focus/blur events don't bubble
-			// through dojo.connect(), and also maybe to catch the focus events early, before onfocus handlers
-			// fire.
-			// Connect to <html> (rather than document) on IE to avoid memory leaks, but document on other browsers because
-			// (at least for FF) the focus event doesn't fire on <html> or <body>.
-			var doc = has("ie") ? targetWindow.document.documentElement : targetWindow.document;
-			if(doc){
-				if(has("ie")){
-					targetWindow.document.body.attachEvent('onmousedown', mousedownListener);
-					var activateListener = function(evt){
-						// IE reports that nodes like <body> have gotten focus, even though they have tabIndex=-1,
-						// ignore those events
-						var tag = evt.srcElement.tagName.toLowerCase();
-						if(tag == "#document" || tag == "body"){ return; }
+			var _this = this,
+				body = targetWindow.document && targetWindow.document.body;
 
+			if(body){
+				var mdh = on(body, 'mousedown', function(evt){
+					_this._justMouseDowned = true;
+					// Use a 13 ms timeout to work-around Chrome resolving too fast and focusout
+					// events not seeing that a mousedown just happened when a popup closes.
+					// See https://bugs.dojotoolkit.org/ticket/17668
+					setTimeout(function(){ _this._justMouseDowned = false; }, 13);
+
+					// workaround weird IE bug where the click is on an orphaned node
+					// (first time clicking a Select/DropDownButton inside a TooltipDialog).
+					// actually, strangely this is happening on latest chrome too.
+					if(evt && evt.target && evt.target.parentNode == null){
+						return;
+					}
+
+					_this._onTouchNode(effectiveNode || evt.target, "mouse");
+				});
+
+				var fih = on(body, 'focusin', function(evt){
+
+					// When you refocus the browser window, IE gives an event with an empty srcElement
+					if(!evt.target.tagName) { return; }
+
+					// IE reports that nodes like <body> have gotten focus, even though they have tabIndex=-1,
+					// ignore those events
+					var tag = evt.target.tagName.toLowerCase();
+					if(tag == "#document" || tag == "body"){ return; }
+
+					if(a11y.isTabNavigable(evt.target)){
+						// If condition doesn't seem quite right, but it is correctly preventing focus events for
+						// clicks on disabled buttons.
+						_this._onFocusNode(effectiveNode || evt.target);
+					}else{
 						// Previous code called _onTouchNode() for any activate event on a non-focusable node.   Can
 						// probably just ignore such an event as it will be handled by onmousedown handler above, but
 						// leaving the code for now.
-						if(a11y.isTabNavigable(evt.srcElement)){
-							_this._onFocusNode(effectiveNode || evt.srcElement);
-						}else{
-							_this._onTouchNode(effectiveNode || evt.srcElement);
-						}
-					};
-					doc.attachEvent('onactivate', activateListener);
-					var deactivateListener =  function(evt){
-						_this._onBlurNode(effectiveNode || evt.srcElement);
-					};
-					doc.attachEvent('ondeactivate', deactivateListener);
+						_this._onTouchNode(effectiveNode || evt.target);
+					}
+				});
 
-					return {
-						remove: function(){
-							targetWindow.document.detachEvent('onmousedown', mousedownListener);
-							doc.detachEvent('onactivate', activateListener);
-							doc.detachEvent('ondeactivate', deactivateListener);
-							doc = null;	// prevent memory leak (apparent circular reference via closure)
-						}
-					};
-				}else{
-					doc.body.addEventListener('mousedown', mousedownListener, true);
-					doc.body.addEventListener('touchstart', mousedownListener, true);
-					var focusListener = function(evt){
-						_this._onFocusNode(effectiveNode || evt.target);
-					};
-					doc.addEventListener('focus', focusListener, true);
-					var blurListener = function(evt){
-						_this._onBlurNode(effectiveNode || evt.target);
-					};
-					doc.addEventListener('blur', blurListener, true);
+				var foh = on(body, 'focusout', function(evt){
+					_this._onBlurNode(effectiveNode || evt.target);
+				});
 
-					return {
-						remove: function(){
-							doc.body.removeEventListener('mousedown', mousedownListener, true);
-							doc.body.removeEventListener('touchstart', mousedownListener, true);
-							doc.removeEventListener('focus', focusListener, true);
-							doc.removeEventListener('blur', blurListener, true);
-							doc = null;	// prevent memory leak (apparent circular reference via closure)
-						}
-					};
-				}
+				return {
+					remove: function(){
+						mdh.remove();
+						fih.remove();
+						foh.remove();
+						mdh = fih = foh = null;
+						body = null;	// prevent memory leak (apparent circular reference via closure)
+					}
+				};
 			}
 		},
 
-		_onBlurNode: function(/*DomNode*/ /*===== node =====*/){
+		_onBlurNode: function(/*DomNode*/ node){
 			// summary:
-			// 		Called when focus leaves a node.
+			//		Called when focus leaves a node.
 			//		Usually ignored, _unless_ it *isn't* followed by touching another node,
 			//		which indicates that we tabbed off the last field on the page,
 			//		in which case every widget is marked inactive
-			this.set("prevNode", this.curNode);
-			this.set("curNode", null);
+
+			// If the blur event isn't followed by a focus event, it means the user clicked on something unfocusable,
+			// so clear focus.
+			if(this._clearFocusTimer){
+				clearTimeout(this._clearFocusTimer);
+			}
+			this._clearFocusTimer = setTimeout(lang.hitch(this, function(){
+				this.set("prevNode", this.curNode);
+				this.set("curNode", null);
+			}), 0);
 
 			if(this._justMouseDowned){
 				// the mouse down caused a new widget to be marked as active; this blur event
@@ -225,14 +172,13 @@ define([
 				return;
 			}
 
-			// if the blur event isn't followed by a focus event then mark all widgets as inactive.
+			// If the blur event isn't followed by a focus or touch event then mark all widgets as inactive.
 			if(this._clearActiveWidgetsTimer){
 				clearTimeout(this._clearActiveWidgetsTimer);
 			}
 			this._clearActiveWidgetsTimer = setTimeout(lang.hitch(this, function(){
 				delete this._clearActiveWidgetsTimer;
 				this._setStack([]);
-				this.prevNode = null;
 			}), 100);
 		},
 
@@ -298,9 +244,17 @@ define([
 				return;
 			}
 
+			// There was probably a blur event right before this event, but since we have a new focus, don't
+			// do anything with the blur
+			if(this._clearFocusTimer){
+				clearTimeout(this._clearFocusTimer);
+				delete this._clearFocusTimer;
+			}
+
 			this._onTouchNode(node);
 
 			if(node == this.curNode){ return; }
+			this.set("prevNode", this.curNode);
 			this.set("curNode", node);
 		},
 
@@ -312,19 +266,19 @@ define([
 			// by:
 			//		"mouse" if the focus/touch was caused by a mouse down event
 
-			var oldStack = this.activeStack;
-			this.set("activeStack", newStack);
+			var oldStack = this.activeStack, lastOldIdx = oldStack.length - 1, lastNewIdx = newStack.length - 1;
 
-			// compare old stack to new stack to see how many elements they have in common
-			for(var nCommon=0; nCommon<Math.min(oldStack.length, newStack.length); nCommon++){
-				if(oldStack[nCommon] != newStack[nCommon]){
-					break;
-				}
+			if(newStack[lastNewIdx] == oldStack[lastOldIdx]){
+				// no changes, return now to avoid spurious notifications about changes to activeStack
+				return;
 			}
 
-			var widget;
+			this.set("activeStack", newStack);
+
+			var widget, i;
+
 			// for all elements that have gone out of focus, set focused=false
-			for(var i=oldStack.length-1; i>=nCommon; i--){
+			for(i = lastOldIdx; i >= 0 && oldStack[i] != newStack[i]; i--){
 				widget = registry.byId(oldStack[i]);
 				if(widget){
 					widget._hasBeenBlurred = true;		// TODO: used by form widgets, should be moved there
@@ -337,7 +291,7 @@ define([
 			}
 
 			// for all element that have come into focus, set focused=true
-			for(i=nCommon; i<newStack.length; i++){
+			for(i++; i <= lastNewIdx; i++){
 				widget = registry.byId(newStack[i]);
 				if(widget){
 					widget.set("focused", true);
@@ -361,18 +315,20 @@ define([
 	var singleton = new FocusManager();
 
 	// register top window and all the iframes it contains
-	ready(function(){
-		var handle = singleton.registerWin(win.doc.parentWindow || win.doc.defaultView);
+	domReady(function(){
+		var handle = singleton.registerWin(winUtils.get(document));
 		if(has("ie")){
-			unload.addOnWindowUnload(function(){
-				handle.remove();
-				handle = null;
-			})
+			on(window, "unload", function(){
+				if(handle){	// because this gets called twice when doh.robot is running
+					handle.remove();
+					handle = null;
+				}
+			});
 		}
 	});
 
 	// Setup dijit.focus as a pointer to the singleton but also (for backwards compatibility)
-	// as a function to set focus.
+	// as a function to set focus.   Remove for 2.0.
 	dijit.focus = function(node){
 		singleton.focus(node);	// indirection here allows dijit/_base/focus.js to override behavior
 	};
