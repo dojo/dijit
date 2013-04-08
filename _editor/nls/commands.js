c08c38a030b79ec49ba768bd2a6637abf7f8591f
Douglas Hays <doughays@dojotoolkit.org>
2013-04-08 15:13:03 +0000

    Refs #16903.  Update default nls locale boolean lists to include bg and uk.
    git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@31178 560b804f-0ae3-0310-86f3-f6aa0a117693
    (cherry picked from commit 12221dbdbb6c36c543aeb776ad1be6ac6210c064)
    (cherry picked from commit 02e0b8d96568d58ab5b2a539d32f5cef5043d5c8)

--- _editor/nls/commands.js
+++ _editor/nls/commands.js
@@ -53,6 +53,7 @@ define({ root:
 ,
 "zh": true,
 "zh-tw": true,
+"uk": true,
 "tr": true,
 "th": true,
 "sv": true,
@@ -80,6 +81,7 @@ define({ root:
 "da": true,
 "cs": true,
 "ca": true,
+"bg": true,
 "az": true,
 "ar": true
 });
