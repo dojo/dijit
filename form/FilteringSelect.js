b0e266d09fc83ae16bdc25a978cf945d2433299a
Ed Hager <ehager@sitepen.com>
2013-09-12 10:22:14 -0700

    Fix a queryOptions problem that occurs when a FilteringSelect loses focus and does a store lookup. Fixes #17434, refs #12373. (cherry picked from commit 2253d0eb61e178fce92d4b9bb68aaad7ad3cade3)

--- form/FilteringSelect.js
+++ form/FilteringSelect.js
@@ -209,8 +209,10 @@ define([
 				this._set("displayedValue", label);	// for watch("displayedValue") notification
 				var _this = this;
 				var options = {
-					ignoreCase: this.ignoreCase,
-					deep: true
+					queryOptions: {
+						ignoreCase: this.ignoreCase,
+						deep: true
+					}
 				};
 				lang.mixin(options, this.fetchProperties);
 				this._fetchHandle = this.store.query(query, options);
