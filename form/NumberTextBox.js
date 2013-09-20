dfa1ea05fb416d5042e736732d2751ca7284ad2a
doughays-dojo <doughays@us.ibm.com>
2013-09-20 07:27:03 -0700

    Refs #17453.  Backport 7e290a2baf2c9e932f618f6cfd191a41f404b997/dijit to 1.9.x.

--- form/NumberTextBox.js
+++ form/NumberTextBox.js
@@ -100,7 +100,7 @@ define([
 		},
 
 		_onFocus: function(){
-			if(this.disabled){ return; }
+			if(this.disabled || this.readOnly){ return; }
 			var val = this.get('value');
 			if(typeof val == "number" && !isNaN(val)){
 				var formattedValue = this.format(val, this.constraints);
