63486c0569d7ba60e351cf01f36dd8f007ed3b48
Heng Liu <liucougar@gmail.com>
2014-03-04 15:55:46 -0800

    fixes #17751 use native placeholder support for form inputs

--- form/TextBox.js
+++ form/TextBox.js
@@ -16,6 +16,10 @@ define([
 	// module:
 	//		dijit/form/TextBox
 
+	has.add('dom-placeholder', function(){
+		return document.createElement('input').placeholder === '';
+	});
+
 	var TextBox = declare("dijit.form.TextBox" + (has("dojo-bidi") ? "_NoBidi" : ""), [_FormValueWidget, _TextBoxMixin], {
 		// summary:
 		//		A base class for textbox form inputs
@@ -63,26 +67,30 @@ define([
 
 		_setPlaceHolderAttr: function(v){
 			this._set("placeHolder", v);
-			if(!this._phspan){
-				this._attachPoints.push('_phspan');
-				this._phspan = domConstruct.create('span', {
-					// dijitInputField class gives placeHolder same padding as the input field
-					// parent node already has dijitInputField class but it doesn't affect this <span>
-					// since it's position: absolute.
-					className: 'dijitPlaceHolder dijitInputField'
-				}, this.textbox, 'after');
-				this.own(
-					on(this._phspan, "mousedown", function(evt){ evt.preventDefault(); }),
-					on(this._phspan, "touchend, pointerup, MSPointerUp", lang.hitch(this, function(){
-						// If the user clicks placeholder rather than the <input>, need programmatic focus.  Normally this
-						// is done in _FormWidgetMixin._onFocus() but after [30663] it's done on a delay, which is ineffective.
-						this.focus();
-					}))
-				);
+			if(has('dom-placeholder')){
+				this.textbox.placeholder = v;
+			}else{
+				if(!this._phspan){
+					this._attachPoints.push('_phspan');
+					this._phspan = domConstruct.create('span', {
+						// dijitInputField class gives placeHolder same padding as the input field
+						// parent node already has dijitInputField class but it doesn't affect this <span>
+						// since it's position: absolute.
+						className: 'dijitPlaceHolder dijitInputField'
+					}, this.textbox, 'after');
+					this.own(
+						on(this._phspan, "mousedown", function(evt){ evt.preventDefault(); }),
+						on(this._phspan, "touchend, pointerup, MSPointerUp", lang.hitch(this, function(){
+							// If the user clicks placeholder rather than the <input>, need programmatic focus.  Normally this
+							// is done in _FormWidgetMixin._onFocus() but after [30663] it's done on a delay, which is ineffective.
+							this.focus();
+						}))
+					);
+				}
+				this._phspan.innerHTML="";
+				this._phspan.appendChild(this._phspan.ownerDocument.createTextNode(v));
+				this._updatePlaceHolder();
 			}
-			this._phspan.innerHTML="";
-			this._phspan.appendChild(this._phspan.ownerDocument.createTextNode(v));
-			this._updatePlaceHolder();
 		},
 
 		_onInput: function(/*Event*/ evt){
@@ -94,7 +102,7 @@ define([
 		},
 
 		_updatePlaceHolder: function(){
-			if(this._phspan){
+			if(!has('dom-placeholder') && this._phspan){
 				this._phspan.style.display = (this.placeHolder && !this.textbox.value) ? "" : "none";
 			}
 		},
@@ -162,7 +170,7 @@ define([
 		}
 	}
 
-	if(has("dojo-bidi")){
+	if(has("dojo-bidi") && !has('dom-placeholder')){
 		TextBox = declare("dijit.form.TextBox", TextBox, {
 			_setPlaceHolderAttr: function(v){
 				this.inherited(arguments);
