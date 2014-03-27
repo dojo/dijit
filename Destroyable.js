0169fa4b37922aaf17cd1ebc034fc00b27353878
Bill Keese <bill@dojotoolkit.org>
2014-03-27 14:34:49 +0900

    Avoid double destroy when handle has multiple destroy methods (ex: destroy() and cancel()).  Fixes #17763.
    Thanks Benjamin Santalucia for patch!

--- Destroyable.js
+++ Destroyable.js
@@ -31,26 +31,39 @@ define([
 			//		The array of specified handles, so you can do for example:
 			//	|		var handle = this.own(on(...))[0];
 
-			array.forEach(arguments, function(handle){
-				var destroyMethodName =
-					"destroyRecursive" in handle ? "destroyRecursive" : // remove "destroyRecursive" for 2.0
-						"destroy" in handle ? "destroy" :
-							"remove" in handle ? "remove" :
-								"cancel";
+			var cleanupMethods = [
+				"destroyRecursive",
+				"destroy",
+				"remove",
+				"cancel"
+			];
 
+			array.forEach(arguments, function(handle){
 				// When this.destroy() is called, destroy handle.  Since I'm using aspect.before(),
 				// the handle will be destroyed before a subclass's destroy() method starts running, before it calls
 				// this.inherited() or even if it doesn't call this.inherited() at all.  If that's an issue, make an
 				// onDestroy() method and connect to that instead.
+				var destroyMethodName;
 				var odh = aspect.before(this, "destroy", function(preserveDom){
 					handle[destroyMethodName](preserveDom);
 				});
 
 				// If handle is destroyed manually before this.destroy() is called, remove the listener set directly above.
-				var hdh = aspect.after(handle, destroyMethodName, function(){
+				// This loop also computes destroyMethodName, used in above listener.
+				var hdhs = [];
+				function onManualDestroy(){
 					odh.remove();
-					hdh.remove();
-				}, true);
+					array.forEach(hdhs, function(hdh){ hdh.remove(); });
+				}
+				array.forEach(cleanupMethods, function(cleanupMethod){
+					if(typeof handle[cleanupMethod] === "function") {
+						if(!destroyMethodName){
+							// Use first matching method name in above listener (prefer destroyRecursive() to destroy())
+							destroyMethodName = cleanupMethod;
+						}
+						hdhs.push(aspect.after(handle, cleanupMethod, onManualDestroy, true));
+					}
+				});
 			}, this);
 
 			return arguments;		// handle
