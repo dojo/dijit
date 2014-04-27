216b1a4658479fd81555dbfd6ae3e7d962ee022c
Lamiaa-Said-IBM <lamiaas@eg.ibm.com>
2014-04-27 19:27:07 +0200

    Update MenuItem.js

--- MenuItem.js
+++ MenuItem.js
@@ -1,6 +1,7 @@
 define([
 	"dojo/_base/declare", // declare
 	"dojo/dom", // dom.setSelectable
+	"dojo/dom-construct",
 	"dojo/dom-attr", // domAttr.set
 	"dojo/dom-class", // domClass.toggle
 	"dojo/_base/kernel", // kernel.deprecated
@@ -11,7 +12,7 @@ define([
 	"./_Contained",
 	"./_CssStateMixin",
 	"dojo/text!./templates/MenuItem.html"
-], function(declare, dom, domAttr, domClass, kernel, has, lang,
+], function(declare, dom, domConstruct, domAttr, domClass, kernel, has, lang,
 			_Widget, _TemplatedMixin, _Contained, _CssStateMixin, template){
 
 	// module:
@@ -96,19 +97,16 @@ define([
 		},
 
 		// Bidi Support
-		postCreate:function()
-		{
+		startup: function(){
+			this.inherited(arguments);
 			if(this.getParent()!= null){
 				var mainMenuDirection = this.getParent().isLeftToRight();//get the parent menu direction
 				var curMenuItemDirection = this.isLeftToRight();
-				if(curMenuItemDirection != mainMenuDirection)
-				{
+				if(curMenuItemDirection != mainMenuDirection){
 					var itemsLength = this.focusNode.children.length;
-					var domConstruct = require("dojo/dom-construct");
-					for(var i =0;i<itemsLength-1;i++)
-						{
+					for(var i =0;i<itemsLength-1;i++){
 						domConstruct.place(this.focusNode.children[i+1],this.focusNode.children[0], "before");
-						}
+					}
 				}
 			}
 		},
