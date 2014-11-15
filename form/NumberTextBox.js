e4ba7db4d7f717656e2d21e03128aad140832db8
Terence Kent <terencekent@gmail.com>
2014-11-14 19:19:57 -0800

    Patch getDecimalInfo to support the range form of the places constraint.
    Fixes 18367

--- form/NumberTextBox.js
+++ form/NumberTextBox.js
@@ -16,10 +16,18 @@ define([
 		var constraints = constraints || {},
 			bundle = i18n.getLocalization("dojo.cldr", "number", i18n.normalizeLocale(constraints.locale)),
 			pattern = constraints.pattern ? constraints.pattern : bundle[(constraints.type || "decimal")+"Format"],
-			placesSpecified = typeof constraints.places == "number",
+			places = typeof constraints.places == "number" ? constraints.places : null;
+
 			// The "places" property trumps the pattern property if both are specified in number.format, we follow the same
-			// logic here
-			places = placesSpecified ? constraints.places : (pattern.indexOf(".") != -1 ? pattern.split(".")[1].replace(/[^#0]/g, "").length : 0);
+		if (places == null){
+			if (typeof constraints.places === "string" && constraints.places.length > 0){
+				var placesArr = constraints.places.split(",");
+				places = placesArr[placesArr.length-1];
+			} else {
+				places = (pattern.indexOf(".") != -1 ? pattern.split(".")[1].replace(/[^#0]/g, "").length : 0);
+			}
+		}
+
 		return { sep: bundle.decimal, places: places };
 	};
 
