a2d409d92bc08bbcf7761e5f1e85dee6e1a33a80
Bill Keese <bill@dojotoolkit.org>
2015-01-04 07:58:57 +0900

    fix typo, fixes #18431

--- Tooltip.js
+++ Tooltip.js
@@ -395,7 +395,7 @@ define([
 			this._set("connectId", newId);
 		},
 
-		addTarget: function(/*OomNode|String*/ node){
+		addTarget: function(/*DomNode|String*/ node){
 			// summary:
 			//		Attach tooltip to specified node if it's not already connected
 
