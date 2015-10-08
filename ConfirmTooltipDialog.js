902bcce3baa30de3ea43df9dc51998afe2f12581
Bill Keese <bill@dojotoolkit.org>
2015-10-08 10:43:59 +0900

    Fix typo in class declarations for ConfirmDialog, ConfirmTooltipDialog. Refs #16930.
    (cherry picked from commit 1da57ea918ab8394b5c0fb0b6b3b084937a8628b)

--- ConfirmTooltipDialog.js
+++ ConfirmTooltipDialog.js
@@ -4,7 +4,7 @@ define([
 	"./_ConfirmDialogMixin"
 ], function(declare, TooltipDialog, _ConfirmDialogMixin) {
 	
-	return declare("dijit/ConfirmTooltipDialog", [TooltipDialog, _ConfirmDialogMixin], {
+	return declare("dijit.ConfirmTooltipDialog", [TooltipDialog, _ConfirmDialogMixin], {
 		// summary:
 		//		A TooltipDialog with OK/Cancel buttons.
 	});
