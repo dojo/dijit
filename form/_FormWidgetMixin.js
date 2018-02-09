a12032055b5ec345ad91c2810f854894f1ecb030
Mike Schall <mike.schall@gmail.com>
2018-02-09 16:22:11 -0600

    Simplify check to use widgets own readOnly property

--- form/_FormWidgetMixin.js
+++ form/_FormWidgetMixin.js
@@ -76,21 +76,9 @@ define([
 			if(/^(button|input|select|textarea|optgroup|option|fieldset)$/i.test(this.focusNode.tagName)){
 				domAttr.set(this.focusNode, 'disabled', value);
 				// IE has a Caret Browsing mode (hit F7 to activate) where disabled textboxes can be modified
-				// textboxes marked readonly avoid this issue.
-				// If setting widget disabled and widget is not already readonly, also set readonly.
-				// If setting widget enabled, and this code set readonly, set to non-readonly
-				if(has("trident")){
-					if(value){
-						if(domAttr.get(this.focusNode, 'readonly') === false){
-							domAttr.set(this.focusNode, 'readonly', true);
-							this._disabledSetReadonly = true;
-						}
-					}else{
-						if(this._disabledSetReadonly){
-							domAttr.set(this.focusNode, 'readonly', false);
-							this._disabledSetReadonly = false;
-						}
-					}
+				// textboxes marked readonly if disabled to avoid this issue.
+				if (has('trident') && 'readOnly' in this) {
+					domAttr.set(this.focusNode, 'readonly', value || this.readOnly);
 				}
 			}else{
 				this.focusNode.setAttribute("aria-disabled", value ? "true" : "false");
